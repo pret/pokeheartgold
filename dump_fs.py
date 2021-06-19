6b55b5483aa5ea70c52e8d748bee95af42be624d
PikalaxALT <pikalaxalt@gmail.com>
2021-06-19 15:44:43 -0400

    Add WIP LSF

--- dump_fs.py
+++ dump_fs.py
@@ -1,6 +1,8 @@
 import struct
 import os
 import subprocess
+import operator
+import itertools
 
 
 class StructReader(struct.Struct):
@@ -16,12 +18,12 @@ def parse_fat(raw_table):
     return list(FATEntry.iter_unpack(raw_table))
 
 
-def parse_fnt(raw_table):
+def parse_fnt(raw_table, root='files'):
     FNTEntry = StructReader('<LHH')
     first_dir, first_file, nfiles = FNTEntry.unpack_from(raw_table)
     assert first_dir == nfiles * FNTEntry.size
     dir_table = list(FNTEntry.iter_unpack(raw_table[:nfiles * FNTEntry.size]))
-    dirs = {0xF000: ['files', None]}
+    dirs = {0xF000: [root, None]}
     files = []
     for i, (offset, file_id, parent_or_count) in enumerate(dir_table):
         if parent_or_count & 0xF000:
@@ -60,29 +62,61 @@ def read_table(rom, ofs):
     return rom.read(size)
 
 
-def dump_overlays(proc, table, allocs, rom):
+def dump_files(dirs, files, allocs, rom):
+    for dir_id, (name, parent, *members) in dirs.items():
+        while parent is not None:
+            name = dirs[parent][0] + '/' + name
+            parent = dirs[parent][1]
+        os.makedirs(name, exist_ok=True)
+        for member in members:
+            if not member & 0xF000:
+                start, end = allocs[member]
+                filename = name + '/' + files[member]
+                with open(filename, 'wb') as ofp:
+                    rom.seek(start)
+                    ofp.write(rom.read(end - start))
+
+
+def dump_overlays(proc, table, allocs, rom, make_files=False):
     for ovy_id, ram_start, size, bsssize, sinit_start, sinit_end, file_id, flag in table:
-        outdir = f'{proc}/overlays/{ovy_id:02d}'
-        os.makedirs(outdir, exist_ok=True)
-        with open(f'{outdir}/module_{ovy_id:02d}.cfg', 'w') as cfg:
-            print('thumb_func', f'0x{ram_start:08X}', f'MOD{ovy_id:02d}_{ram_start:08X}', file=cfg)
-        start, end = allocs[file_id]
-        rom.seek(start)
-        raw_ovy = rom.read(end - start)
-        with open(f'{outdir}/module_{ovy_id:02d}.sbin', 'wb') as ofp:
-            ofp.write(raw_ovy)
-        sbp_args = [
-            '../ndsdisasm/ndsdisasm',
-            '-Du', f'{outdir}/module_{ovy_id:02d}.sbin',
-            '-m', str(ovy_id),
-            '-c', f'{outdir}/module_{ovy_id:02d}.cfg',
-            '-d',
-            'baserom.nds'
-        ]
-        if proc == 'arm7':
-            sbp_args.append('-7')
-        with open(f'{outdir}/module_{ovy_id:02d}.s', 'w') as ofp:
-            subprocess.run(sbp_args, stdout=ofp)
+        if make_files:
+            outdir = f'{proc}/overlays/{ovy_id:02d}'
+            os.makedirs(outdir, exist_ok=True)
+            with open(f'{outdir}/module_{ovy_id:02d}.cfg', 'w') as cfg:
+                print('thumb_func', f'0x{ram_start:08X}', f'MOD{ovy_id:02d}_{ram_start:08X}', file=cfg)
+            start, end = allocs[file_id]
+            rom.seek(start)
+            raw_ovy = rom.read(end - start)
+            with open(f'{outdir}/module_{ovy_id:02d}.sbin', 'wb') as ofp:
+                ofp.write(raw_ovy)
+            sbp_args = [
+                '../ndsdisasm/ndsdisasm',
+                '-Du', f'{outdir}/module_{ovy_id:02d}.sbin',
+                '-m', str(ovy_id),
+                '-c', f'{outdir}/module_{ovy_id:02d}.cfg',
+                '-d',
+                'baserom.nds'
+            ]
+            if proc == 'arm7':
+                sbp_args.append('-7')
+            with open(f'{outdir}/module_{ovy_id:02d}.s', 'w') as ofp:
+                subprocess.run(sbp_args, stdout=ofp)
+        else:
+            print('Overlay', ovy_id)
+            print('{')
+            print('    Address', hex(ram_start))
+            print('    ### Size', hex(size), '###')
+            print('    ### BSS Size', hex(bsssize), '###')
+            print('    ### SINIT Start', hex(sinit_start), '###')
+            print('    ### SINIT End', hex(sinit_end), '###')
+            print('    ### Compressed?', (flag & 0x01000000) != 0, '###')
+            if (flag & 0x01000000) != 0:
+                print('    ### Compressed size', hex(flag & 0x00FFFFFF), '###')
+            print('}')
+            print('')
+
+            for key, values in itertools.groupby(sorted(table, key=operator.itemgetter(1, 0)), key=operator.itemgetter(1)):
+                print(hex(key), [x[0] for x in values])
 
 
 def main():
@@ -97,18 +131,7 @@ def main():
         ovy9 = parse_overlays(ovy9_raw)
         ovy7 = parse_overlays(ovy7_raw)
 
-        for dir_id, (name, parent, *members) in dirs.items():
-            while parent is not None:
-                name = dirs[parent][0] + '/' + name
-                parent = dirs[parent][1]
-            os.makedirs(name, exist_ok=True)
-            for member in members:
-                if not member & 0xF000:
-                    start, end = allocs[member]
-                    filename = name + '/' + files[member]
-                    with open(filename, 'wb') as ofp:
-                        rom.seek(start)
-                        ofp.write(rom.read(end - start))
+        # dump_files(dirs, files, allocs, rom)
 
         dump_overlays('.', ovy9, allocs, rom)
         dump_overlays('sub', ovy7, allocs, rom)
