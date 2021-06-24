7f9fe450fcb682c109779d89d2ee50a1df52474a
PikalaxALT <pikalaxalt@gmail.com>
2021-06-24 18:02:38 -0400

    Dump overlay 20

---
+++ fix_ov.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+OV=$1
+
+echo "sed -r -f <(grep -w thumb_func_start overlays/${OV}/module_${OV}.dump.s | grep -oE \"\w+$\" | xargs | sed -r -e 's/ FUN_/|/g' -e 's/^(MOD'${OV}')_/s\/\(FUN\|\1\)_\(/' -e 's/$/\)\/ov'${OV}'_\\2\/g/') overlays/${OV}/module_${OV}.dump.s > overlays/${OV}/module_${OV}.s"
+sed -r -f <(grep -w thumb_func_start overlays/${OV}/module_${OV}.dump.s | grep -oE "\w+$" | xargs | sed -r -e 's/ FUN_/|/g' -e 's/^(MOD'${OV}')_/s\/\(FUN\|\1\)_\(/' -e 's/$/\)\/ov'${OV}'_\\2\/g/') overlays/${OV}/module_${OV}.dump.s > overlays/${OV}/module_${OV}.s
