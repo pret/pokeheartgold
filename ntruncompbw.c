9bf6da4f1d8d843c20528b861a3d50b2d00fd184
PikalaxALT <pikalaxalt@gmail.com>
2021-08-09 12:17:33 -0400

    Clean up ntruncompbw

--- ntruncompbw.c
+++ ntruncompbw.c
@@ -10,55 +10,40 @@ static inline uint32_t READ32(const unsigned char * ptr)
 static uint32_t MIi_UncompressBackwards(unsigned char ** out_p, size_t compsize)
 {
     unsigned char * out = *out_p;
-    //     stmfd sp!, {r4-r7}
+
     // Read the pointer to the end of the compressed image
-    uint8_t * endptr = out + compsize - 8; //     ldmdb r0, {r1, r2}
+    uint8_t * endptr = out + compsize - 8;
     uint32_t size = READ32(endptr);
     uint32_t offset = READ32(endptr + 4);
-
     out = realloc(out, compsize + offset);
-    if (out == NULL) {
+    if (out == NULL)
         return -1u;
-    }
     endptr = out + compsize;
-    uint8_t * dest_p = endptr + offset;                                        //     add r2, r0, r2
-    uint8_t * end = endptr - ((uint8_t)(size >> 24));                          //     sub r3, r0, r1, lsr #24
-    //     bic r1, r1, #0xff000000
-    uint8_t * start = endptr - (size & ~0xFF000000);                           //     sub r1, r0, r1
-    // uint8_t * dest_end = dest_p;
-    //     mov r4, r2 ; not crucial
-    // @.loop:
-    while (end > start)                                                        //     cmp r3, r1
-    {                                                                          //     ble @.dc_flush
-        uint8_t r5 = *--end;                                                   //     ldrb r5, [r3, #-1]!
-        //     mov r6, #8
-        // @.byte_loop:
-        for (int i = 0; i < 8; i++)                                            //     subs r6, r6, #1
-        {                                                                      //     blt @.loop
-            if ((r5 & 0x80) == 0)                                              //     tst r5, #0x80
-            {                                                                  //     bne @.readback
-            //     ldrb r0, [r3, #-1]!
-                *--dest_p = *--end;                                            //     strb r0, [r2, #-1]!
-            }                                                                  //     b @.byte_after
-            else                                                               // @.readback:
-            {
-                int ip = *--end;                                               //     ldrb r12, [r3, #-1]!
-                int r7 = *--end;                                               //     ldrb r7, [r3, #-1]!
-                //     orr r7, r7, r12, lsl #8
-                //     bic r7, r7, #0xf000
-                r7 = ((r7 | (ip << 8)) & ~0xF000) + 2;                         //     add r7, r7, #0x0002
-                ip += 0x20;                                                    //     add r12, r12, #0x0020
-                while (ip >= 0)                                                // @.readback_loop:
-                {
-                    dest_p[-1] = dest_p[r7];                                   //     ldrb r0, [r2, r7]
-                    dest_p--;                                                  //     strb r0, [r2, -1]!
-                    ip -= 0x10;                                                //     subs r12, r12, #0x0010
-                }                                                              //     bge @.readback_loop
-            }                                                                  // @.byte_after:
-            if (end <= start)                                                  //     cmp r3, r1
-                break;                                                         //     mov r5, r5, lsl #1
-                r5 <<= 1;                                                          //     bgt @.byte_loop
-        }                                                                      // @.dc_flush:
+    uint8_t * dest_p = endptr + offset;
+    uint8_t * end = endptr - ((uint8_t)(size >> 24));
+    uint8_t * start = endptr - (size & ~0xFF000000);
+    while (end > start) {
+        uint8_t r5 = *--end;
+        for (int i = 0; i < 8; i++) {
+            if ((r5 & 0x80) == 0)
+                *--dest_p = *--end;
+            else {
+                int ip = *--end;
+                int r7 = *--end;
+
+
+                r7 = ((r7 | (ip << 8)) & ~0xF000) + 2;
+                ip += 0x20;
+                while (ip >= 0) {
+                    dest_p[-1] = dest_p[r7];
+                    dest_p--;
+                    ip -= 0x10;
+                }
+            }
+            if (end <= start)
+                break;
+                r5 <<= 1;
+        }
     }
     *out_p = out;
     return compsize + offset;
