d202789dad192eb9da6d81369334e584d1bf1fff
PikalaxALT <pikalaxalt@gmail.com>
2021-08-11 17:53:04 -0400

    Temp workaround for differing arcs

--- filesystem.mk
+++ filesystem.mk
@@ -388,4 +388,13 @@ NITROFS_FILES := \
 filesystem: $(NITROFS_FILES)
 
 # TODO: file rules
-$(NITROFS_FILES): ;
+# Some filenames are stripped and replaced with a serial number
+# such that the XYZth file is mapped to a/X/Y/Z.
+# Temporary names for now
+DIFF_ARCS := files/a/0/7/5 files/a/1/3/3 files/a/2/5/2
+$(DIFF_ARCS): files/%: files/%.$(buildname)
+	cp $< $@
+
+.PHONY: $(DIFF_ARCS)
+
+$(filter-out $(DIFF_ARCS),$(NITROFS_FILES)): ;
