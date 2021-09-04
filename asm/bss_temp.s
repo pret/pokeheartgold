d00cfed13051c3dee370682331b3f461274769a6
PikalaxALT <pikalaxalt@gmail.com>
2021-09-04 11:18:34 -0400

    Carve out 201FFE0, 202055C

--- asm/bss_temp.s
+++ asm/bss_temp.s
@@ -1,8 +1,8 @@
 ; Temporary file to declare the static BSS section for ARM9
 	.bss
 
-_021D15A4:
-	.space 0x14344
+_021D2194:
+	.space 0x13754
 
 	.public __global_destructor_chain
 __global_destructor_chain: ; 0x021E58E8
