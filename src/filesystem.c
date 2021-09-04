ba5656cc12231f81de73c9befd823b5de0e85411
PikalaxALT <pikalaxalt@gmail.com>
2021-09-04 15:00:53 -0400

    Finish splitting code between GX_layers and error_handling

--- src/filesystem.c
+++ src/filesystem.c
@@ -2,7 +2,6 @@
 
 #include "global.h"
 #include "filesystem.h"
-#include "msgdata/msg.naix"
 
 static void ReadFromNarcMemberByPathAndId(void * dest, const char * path, s32 file_idx, u32 offset, u32 size)
 {
