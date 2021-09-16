dbb7e5723df58bd846c3e3e3ebb6b6288e3b8be5
PikalaxALT <pikalaxalt@gmail.com>
2021-09-16 16:07:39 -0400

    Split through 205B3DC

--- asm/bss_temp.s
+++ asm/bss_temp.s
@@ -1,8 +1,8 @@
 ; Temporary file to declare the static BSS section for ARM9
 	.bss
 
-_021D41C4:
-	.space 0x11724
+_021D41CC:
+	.space 0x1171C
 
 	.public __global_destructor_chain
 __global_destructor_chain: ; 0x021E58E8
