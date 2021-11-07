b40d0f1f4e9f7122b1531fe8b05bba25f2f73be5
tgsm <doodrabbit@hotmail.com>
2021-11-07 03:14:27 -0500

    nitro: Add RTCWeek enum and RTCDate struct

---
+++ lib/include/nitro/rtc/api.h
@@ -0,0 +1,21 @@
+#ifndef NITRO_RTC_API_H_
+#define NITRO_RTC_API_H_
+
+typedef enum {
+    RTC_WEEK_SUNDAY,
+    RTC_WEEK_MONDAY,
+    RTC_WEEK_TUESDAY,
+    RTC_WEEK_WEDNESDAY,
+    RTC_WEEK_THURDDAY,
+    RTC_WEEK_FRIDAY,
+    RTC_WEEK_SATURDAY,
+} RTCWeek;
+
+typedef struct {
+    u32 year;
+    u32 month;
+    u32 day;
+    RTCWeek day_of_week;
+} RTCDate;
+
+#endif //NITRO_RTC_API_H_
