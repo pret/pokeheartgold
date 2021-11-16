99f84e176e0d8229b34aa7b330fac64a8c33ed0a
PikalaxALT <pikalaxalt@gmail.com>
2021-11-16 13:12:24 -0500

    Reinstate csv2bin global exception handler

--- tools/csv2bin/main.cpp
+++ tools/csv2bin/main.cpp
@@ -1,14 +1,17 @@
 #include "Options.h"
 
 int main(int argc, char ** argv) {
-//    try {
+    try {
         return Options(argc, argv).main();
-//    } catch (argument_error const &e ) {
-//        Options::short_usage(std::cerr);
-//        std::cerr << "csv2bin error: " << e.what() << std::endl;
-//        return 1;
-//    } catch (std::exception const &e) {
-//        std::cerr << "csv2bin error: " << e.what() << std::endl;
-//        return 1;
-//    }
+    } catch (std::bad_alloc const &e ) {
+        std::cerr << "csv2bin fatal error: bad alloc: " << e.what() << std::endl;
+        return 1;
+    } catch (argument_error const &e ) {
+        Options::short_usage(std::cerr);
+        std::cerr << "csv2bin argument error: " << e.what() << std::endl;
+        return 1;
+    } catch (std::exception const &e) {
+        std::cerr << "csv2bin error: " << e.what() << std::endl;
+        return 1;
+    }
 }
