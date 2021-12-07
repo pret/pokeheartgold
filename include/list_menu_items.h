ef27e1369043985775a91325c45aa6f3998b9889
PikalaxALT <pikalaxalt@gmail.com>
2021-12-06 21:08:30 -0500

    list_menu_items.c

---
+++ include/list_menu_items.h
@@ -0,0 +1,18 @@
+#ifndef POKEHEARTGOLD_LIST_MENU_ITEMS_H
+#define POKEHEARTGOLD_LIST_MENU_ITEMS_H
+
+#include "pm_string.h"
+#include "heap.h"
+#include "msgdata.h"
+
+typedef struct LISTMENUITEM {
+    STRING *text;
+    s32 value;
+} LISTMENUITEM;
+
+LISTMENUITEM *ListMenuItems_ctor(u32 n, HeapID heapId);
+void ListMenuItems_dtor(LISTMENUITEM *items);
+void ListMenuItems_AppendFromMsgData(LISTMENUITEM *items, MSGDATA *msgData, int msgId, int value);
+void ListMenuItems_AddItem(LISTMENUITEM *items, STRING *string, int value);
+
+#endif //POKEHEARTGOLD_LIST_MENU_ITEMS_H
