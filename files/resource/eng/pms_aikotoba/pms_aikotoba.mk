52277ab0d16af4e1512f38e6853d4bfa3efd6e2e
PikalaxALT <pikalaxalt@gmail.com>
2021-12-16 10:12:18 -0500

    Dump event data to JSON

--- files/resource/eng/pms_aikotoba/pms_aikotoba.mk
+++ files/resource/eng/pms_aikotoba/pms_aikotoba.mk
@@ -1,6 +1,8 @@
 PMS_AIKOTOBA_S    := files/resource/eng/pms_aikotoba/pms_aikotoba.s
 PMS_AIKOTOBA_NARC := $(PMS_AIKOTOBA_S:%.s=%.narc)
 
+FS_CLEAN_TARGETS += $(PMS_AIKOTOBA_NARC) $(PMS_AIKOTOBA_S:%.s=%.o) $(PMS_AIKOTOBA_S:%.s=%)
+
 
 ifeq ($(NODEP),)
 PMS_AIKOTOBA_DEP  := $(PMS_AIKOTOBA_S:%.s=%.d)
@@ -17,6 +19,7 @@ $(PMS_AIKOTOBA_NARC): %.narc: %.s %.d
 $(PMS_AIKOTOBA_DEP):
 
 include $(wildcard $(PMS_AIKOTOBA_DEP))
+FS_CLEAN_TARGETS += $(PMS_AIKOTOBA_DEP)
 else
 $(PMS_AIKOTOBA_NARC): %.narc: %.s
 	@echo gen  $@
@@ -27,5 +30,3 @@ $(PMS_AIKOTOBA_NARC): %.narc: %.s
 	@$(OBJCOPY) -O binary $*.o $*/tmp.bin
 	@$(KNARC) -p $@ -d $*
 endif
-
-.INTERMEDIATE: $(PMS_AIKOTOBA_S:%.s=%.o) $(PMS_AIKOTOBA_S:%.s=%/tmp.bin)
