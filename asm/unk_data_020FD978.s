f7c1e8e2a2397dfb31202e115ae1042796478908
PikalaxALT <pikalaxalt@gmail.com>
2022-01-21 14:19:01 -0500

    Carve out a bit more from 0205C268

---
+++ asm/unk_data_020FD978.s
@@ -0,0 +1,96 @@
+#include "constants/sndseq.h"
+#include "constants/species.h"
+#include "constants/maps.h"
+#include "constants/pokemon.h"
+#include "constants/flags.h"
+#include "constants/vars.h"
+#include "constants/items.h"
+#include "constants/std_script.h"
+#include "fielddata/script/scr_seq/event_D24R0204.h"
+#include "party_menu.h"
+	.include "asm/macros.inc"
+	.include "global.inc"
+
+	.rodata
+
+_020FD978:
+	.word sub_020624A8
+	.word sub_02062470
+
+_020FD980:
+	.word sub_020624C0
+	.word sub_02062470
+
+_020FD988:
+	.word sub_02062F18
+	.word sub_02062470
+
+_020FD990:
+	.word sub_02062F30
+	.word sub_02062470
+
+_020FD998:
+	.word sub_02062F04
+	.word sub_02062470
+
+_020FD9A0:
+	.word sub_02062ED8
+	.word sub_02062470
+
+_020FD9A8:
+	.word sub_02062EC0
+	.word sub_02062470
+
+_020FD9B0:
+	.word sub_0206249C
+	.word sub_02062470
+
+_020FD9B8:
+	.word sub_020624B4
+	.word sub_02062470
+
+_020FD9C0:
+	.word sub_02062EF0
+	.word sub_02062470
+
+_020FD9C8:
+	.word sub_020625E0
+	.word sub_02062514
+	.word sub_02062470
+
+_020FD9D4:
+	.word sub_02062770
+	.word sub_02062514
+	.word sub_02062470
+
+_020FD9E0:
+	.word sub_020630D4
+	.word sub_020630E4
+	.word sub_02062470
+
+_020FD9EC:
+	.word sub_02062590
+	.word sub_02062514
+	.word sub_02062470
+
+_020FD9F8:
+	.word sub_02062798
+	.word sub_02062514
+	.word sub_02062470
+
+_020FDA04:
+	.word sub_02062568
+	.word sub_02062514
+	.word sub_02062470
+
+_020FDA10:
+	.word sub_020625F4
+	.word sub_02062514
+	.word sub_02062470
+
+	.data
+
+_0210FACC:
+	.word _020FDA84
+	.word _020FDAC4
+	.word _020FDB04
