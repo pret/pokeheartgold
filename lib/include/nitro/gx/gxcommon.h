8124d2adaf3ad3d33ced62be8f9f27136679ba92
PikalaxALT <pikalaxalt@gmail.com>
2022-03-08 21:03:14 -0500

    Fix GX_RGB incompat with asm

--- lib/include/nitro/gx/gxcommon.h
+++ lib/include/nitro/gx/gxcommon.h
@@ -11,6 +11,10 @@ typedef u16 GXRgb;
 #define GX_RGB_G_MASK             (0x03e0)
 #define GX_RGB_B_SHIFT            (10)
 #define GX_RGB_B_MASK             (0x7c00)
+#ifdef SDK_ASM
+#define GX_RGB(r, g, b)           ((((r) << GX_RGB_R_SHIFT) | ((g) << GX_RGB_G_SHIFT) | ((b) << GX_RGB_B_SHIFT)))
+#else
 #define GX_RGB(r, g, b)           ((GXRgb)(((r) << GX_RGB_R_SHIFT) | ((g) << GX_RGB_G_SHIFT) | ((b) << GX_RGB_B_SHIFT)))
+#endif
 
 #endif //NITRO_GX_GXCOMMON_H_
