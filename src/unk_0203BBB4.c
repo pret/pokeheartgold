5496886ff497fbe1f22b2e546f321246f8960947
Adrienn Tindall <33725376+aidenfrog@users.noreply.github.com>
2022-03-12 13:59:01 -0600

    Added requested style changes

--- src/unk_0203BBB4.c
+++ src/unk_0203BBB4.c
@@ -14,20 +14,20 @@ const struct MonthDay DiamondDustDates[] = {
 
 u32 sub_0203BBB4(FieldSystem* fsys, u32 mapId) {
     u8 c;
-	
-	u32 weatherType = MapHeader_GetWeatherType(mapId);
-    if(MAP_D41R0108 != mapId) {
-		return weatherType;
-	}
+    
+    u32 weatherType = MapHeader_GetWeatherType(mapId);
+    if (MAP_D41R0108 != mapId) {
+        return weatherType;
+    }
     SYSINFO_RTC* sysinfo_rtc = Sav2_SysInfo_RTC_get(fsys->savedata);
-    for(c = 0; c < 8; c++) {
+    for (c = 0; c < 8; c++) {
        u8 month = DiamondDustDates[c].month;
        u8 day = DiamondDustDates[c].day;
-       if((sysinfo_rtc->date.month == month) && (sysinfo_rtc->date.day == day)) { 
-			if(!sub_02055670(fsys)) {
-				weatherType = MAP_WEATHER_DIAMOND_DUST;
-			}
-			return weatherType;
+       if ((sysinfo_rtc->date.month == month) && (sysinfo_rtc->date.day == day)) { 
+            if (!sub_02055670(fsys)) {
+                weatherType = MAP_WEATHER_DIAMOND_DUST;
+            }
+            return weatherType;
        }
     }
     return weatherType;
