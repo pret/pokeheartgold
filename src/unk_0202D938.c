5cf70c15fc5002f2b9668e132a0d80775364aa96
SinaMegapolis <sinasaffari6@gmail.com>
2022-03-21 03:19:41 +0330

    make a header file + new struct

--- src/unk_0202D938.c
+++ src/unk_0202D938.c
@@ -1,26 +1,30 @@
+#include "unk_0202D938.h"
 #include "save.h"
 
+#define SAVE_UNK_20 0x14
+
 u32 sub_0202D938(void) {
-    return 0xe;
+    return sizeof(UnkSavStruct20);
 }
 
-void sub_0202D93C(u8* argP) {
-    argP[0] = 0;
-    argP[1] = 0;
-    argP[2] = 0;
-    argP[3] = 0;
-    argP[4] = 0;
-    argP[5] = 0;
-    argP[6] = 0;
-    argP[7] = 0;
-    argP[8] = 0;
-    argP[9] = 0;
-    argP[10] = 0;
-    argP[11] = 0;
-    argP[12] = 0;
-    argP[13] = 0;
+void sub_0202D93C(UnkSavStruct20* ptr) {
+    UnkSavStruct20 unk_struct = *ptr;
+    unk_struct.filler_00[0] = 0;
+    unk_struct.filler_00[1] = 0;
+    unk_struct.filler_00[2] = 0;
+    unk_struct.filler_00[3] = 0;
+    unk_struct.filler_00[4] = 0;
+    unk_struct.filler_00[5] = 0;
+    unk_struct.filler_00[6] = 0;
+    unk_struct.filler_00[7] = 0;
+    unk_struct.filler_00[8] = 0;
+    unk_struct.filler_00[9] = 0;
+    unk_struct.filler_00[10] = 0;
+    unk_struct.filler_00[11] = 0;
+    unk_struct.filler_00[12] = 0;
+    unk_struct.filler_00[13] = 0;
 }
 
-void* sub_0202D95C(SAVEDATA* data) {
-    return SavArray_get(data, 0x14);
+UnkSavStruct20* sub_0202D95C(SAVEDATA* data) {
+    return SavArray_get(data, SAVE_UNK_20);
 }
