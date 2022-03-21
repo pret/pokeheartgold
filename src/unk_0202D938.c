6c58634c9f5c71ffa72b3c24e138a54f6cee5865
SinaMegapolis <sinasaffari6@gmail.com>
2022-03-22 02:21:47 +0330

    Changed pointer syntax per request

--- src/unk_0202D938.c
+++ src/unk_0202D938.c
@@ -7,20 +7,20 @@ u32 sub_0202D938(void) {
 }
 
 void sub_0202D93C(UnkSavStruct20* ptr) {
-    (*ptr).filler_00[0] = 0;
-    (*ptr).filler_00[1] = 0;
-    (*ptr).filler_00[2] = 0;
-    (*ptr).filler_00[3] = 0;
-    (*ptr).filler_00[4] = 0;
-    (*ptr).filler_00[5] = 0;
-    (*ptr).filler_00[6] = 0;
-    (*ptr).filler_00[7] = 0;
-    (*ptr).filler_00[8] = 0;
-    (*ptr).filler_00[9] = 0;
-    (*ptr).filler_00[10] = 0;
-    (*ptr).filler_00[11] = 0;
-    (*ptr).filler_00[12] = 0;
-    (*ptr).filler_00[13] = 0;
+    ptr->filler_00[0] = 0;
+    ptr->filler_00[1] = 0;
+    ptr->filler_00[2] = 0;
+    ptr->filler_00[3] = 0;
+    ptr->filler_00[4] = 0;
+    ptr->filler_00[5] = 0;
+    ptr->filler_00[6] = 0;
+    ptr->filler_00[7] = 0;
+    ptr->filler_00[8] = 0;
+    ptr->filler_00[9] = 0;
+    ptr->filler_00[10] = 0;
+    ptr->filler_00[11] = 0;
+    ptr->filler_00[12] = 0;
+    ptr->filler_00[13] = 0;
 }
 
 UnkSavStruct20* sub_0202D95C(SAVEDATA* data) {
