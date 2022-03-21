b9cebd3c363434b42fae9b3c17dfb66ec5ccd2ad
Admiral Fish <admiralfish420@gmail.com>
2022-03-20 21:50:15 -0700

    Port sav_chatot from pokediamond

--- include/sav_chatot.h
+++ include/sav_chatot.h
@@ -3,11 +3,20 @@
 
 #include "save.h"
 
-typedef struct SOUND_CHATOT SOUND_CHATOT;
+typedef struct SOUND_CHATOT {
+    BOOL exists;
+    s8 data[1000];
+} SOUND_CHATOT;
 
-SOUND_CHATOT *Sav2_Chatot_get(SAVEDATA *saveData);
 u32 Sav2_Chatot_sizeof(void);
 void Sav2_Chatot_init(SOUND_CHATOT *chatot);
+SOUND_CHATOT *Chatot_new(HeapID heapId);
+SOUND_CHATOT *Sav2_Chatot_get(SAVEDATA *saveData);
+BOOL Chatot_exists(SOUND_CHATOT *chatot);
 void Chatot_invalidate(SOUND_CHATOT *chatot);
+s8 *Chatot_GetData(SOUND_CHATOT *chatot);
+void Chatot_Decode(s8 *dest, const s8 *data);
+void Chatot_Encode(SOUND_CHATOT *chatot, const s8 *data);
+void Chatot_copy(SOUND_CHATOT *dst, SOUND_CHATOT *src);
 
 #endif //POKEHEARTGOLD_SAV_CHATOT_H
