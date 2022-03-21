cb975680cb624af5f635cdbf9de4d861063d744d
SinaMegapolis <sinasaffari6@gmail.com>
2022-03-22 03:14:38 +0330

    TIL memset exists, so i used that

--- src/unk_0202D938.c
+++ src/unk_0202D938.c
@@ -7,20 +7,7 @@ u32 sub_0202D938(void) {
 }
 
 void sub_0202D93C(UnkSavStruct20* ptr) {
-    ptr->filler_00[0] = 0;
-    ptr->filler_00[1] = 0;
-    ptr->filler_00[2] = 0;
-    ptr->filler_00[3] = 0;
-    ptr->filler_00[4] = 0;
-    ptr->filler_00[5] = 0;
-    ptr->filler_00[6] = 0;
-    ptr->filler_00[7] = 0;
-    ptr->filler_00[8] = 0;
-    ptr->filler_00[9] = 0;
-    ptr->filler_00[10] = 0;
-    ptr->filler_00[11] = 0;
-    ptr->filler_00[12] = 0;
-    ptr->filler_00[13] = 0;
+    memset(ptr, 0, sizeof(UnkSavStruct20));
 }
 
 UnkSavStruct20* sub_0202D95C(SAVEDATA* data) {
