9f597e106a9d47006422ed3807c5696720de1e6e
SinaMegapolis <sinasaffari6@gmail.com>
2022-03-28 00:24:17 +0430

    unk_02092BE8.c 1

--- include/unk_02092BE8.h
+++ include/unk_02092BE8.h
@@ -6,29 +6,11 @@
 #include "script.h"
 #include "gear_phone.h"
 
-/*
-    all Unkstruct_2c offsets:
-    //0x00 (u8???) some bool var
-    //0x01 One of the variables inside FieldSystem with offset 0x110 (currently u8 filler_10C[1];)
-    //0x02 (u16?) Map music ID (surf-overriden)
-    //0x04 (u8?????) UnkFsysSub_114*
-    //0x05 (u8???) related to int x????
-    //0x06 (u8???) also related to int y?????
-    //0x07 u8 player gender
-    //0x08 int x
-    //0x0c int y
-    //0x10 (u32???) map_number???
-    //0x12 u16 map_header
-    //0x22 bitfield for map matrix x (r1 = (((r1 >> 4) >> 27) + r1) >> 5;) (needs more research)
-    //0x23 bitfield for map matrix y (r1 = (((r1 >> 4) >> 27) + r1) >> 5;) (needs more research)
-    //0x24 SAVEDATA*
-    //0x28 pointer to a Field System variable at offset 0x10C (currently u8 filler_10C[0];)
-*/
 typedef struct Unkstruct_2c{
     u8 Unk_Bool00; //0x00
-    u8 Unk_FsysVar01; //0x01
-    u16 Map_MusicID; //0x02
-    u8 UnkFsysSub_114ptr; //0x04
+    u8 Unk_FsysVar01; //0x01 One of the variables inside FieldSystem with offset 0x110 (currently u8 filler_10C[1];)
+    u16 Map_MusicID; //0x02 (surf-overriden)
+    u8 UnkFsysSub_114ptr; //0x04 (u8?????) UnkFsysSub_114*
     u8 Unk_Var05; //0x05
     u8 Unk_Var06; //0x06
     u8 Player_Gender; //0x07
@@ -36,32 +18,16 @@ typedef struct Unkstruct_2c{
     int y; //0x0c
     u16 mapID; //0x10
     u16 mapHeader; //0x12
-    u8 filler_01[0x08];//0x14
-    u8 Unk_Bitfield22; //0x22
-    u8 Unk_Bitfield23; //0x23
+    u8 filler_01[0xe];//0x14
+    u8 MatrixXCoord; //0x22
+    u8 MatrixYCoord; //0x23
     SAVEDATA* data; //0x24
-    int Unk_ptr28; //0x28
+    int Unk_ptr28; //0x28 pointer to a Field System variable at offset 0x10C (currently u8 filler_10C[0];)
 }UnkSTRUCT_2C;
 
-/*
-    (local struct)
-    all Unkstruct_14 offsets as they appear in the function below:
-    //SP + 0x00 BOOL var
-    //SP + 0x04 Location * PosPtr
-    //SP + 0x08 u16 *x
-    //SP + 0x0a u16 *y
-    //SP + 0x0c Unk_Var0c
-    //SP + 0x0d Unk_Var0d
-    //SP + 0x0e Unk_Var0e
-    //SP + 0x0f Unk_Var0f
-    //SP + 0x10 Unk_Var10
-    //SP + 0x11 Unk_Arr11
-*/
 typedef struct Unkstruct_14{
-    BOOL Unkvar0;
-    Location *PosPtr;
-    u16 *x;
-    u16 *y;
+    s16 y;
+    s16 x;
     u8 Unk_Var0c;
     u8 Unk_Var0d;
     u8 Unk_Var0e;
@@ -73,12 +39,12 @@ typedef struct Unkstruct_14{
 void sub_02092BE8(FieldSystem* sys, UnkSTRUCT_2C* ptr, BOOL arg3);
 void sub_02092D80(FieldSystem* sys, UnkSTRUCT_2C* ptr);
 void sub_02092D8C(FieldSystem* sys, UnkSTRUCT_2C* ptr);
-void sub_02092D98(HeapID id, FieldSystem* sys);
-void sub_02092DD8(void* UnkPtr);
+struct UnkFsysSub_114* sub_02092D98(HeapID id, FieldSystem* sys);
+void sub_02092DD8(struct UnkFsysSub_114 * ptr);
 struct UnkFsysSub_114* sub_02092DEC(FieldSystem* sys);
 void sub_02092DF4(struct UnkFsysSub_114* ptr);
 u8 sub_02092E08(struct UnkFsysSub_114* ptr);
-u8 sub_02092E10(struct UnkFsysSub_114* ptr);
+struct PhoneBookEntry* sub_02092E10(struct UnkFsysSub_114* ptr);
 void sub_02092E14(struct UnkFsysSub_114* ptr, u32 Unkarg1, u32 Unkarg2);
 void sub_02092E34(struct UnkFsysSub_114* ptr, u32 Unkarg1, u32 Unkarg2);
 void sub_02092E54(struct UnkFsysSub_114* ptr);
--- src/unk_02092BE8.c
+++ src/unk_02092BE8.c
@@ -1,3 +1,120 @@
 #include "unk_02092BE8.h"
 #include "safari_zone.h"
 #include "constants/sndseq.h"
+
+void sub_02092BE8(FieldSystem* sys, UnkSTRUCT_2C* ptr, BOOL arg3) { //r5, r4, SP
+    UnkSTRUCT_14 s;
+    SCRIPT_STATE *state = SavArray_Flags_get(sys->savedata);
+    FLYPOINTS_SAVE *points = Save_FlyPoints_get(sys->savedata); 
+    Location *warpPtr = FlyPoints_GetSpecialSpawnWarpPtr(points); //r6
+    Location *PosPtr = FlyPoints_GetPosition(points); //SP + 4
+    struct UnkFsysSub_114* Unk_V1 = sub_02092DEC(sys); //r7
+    ptr->data = sys->savedata;
+    ptr->mapID = PosPtr->mapId;
+    if(MapHeader_MapIsOnMainMatrix(ptr->mapID) != 0){ 
+        ptr->x = GetPlayerXCoord(sys->playerAvatar);
+        ptr->y = GetPlayerYCoord(sys->playerAvatar);
+        ptr->MatrixXCoord = ptr->x / 32;
+        ptr->MatrixYCoord = ptr->y / 32;
+        ptr->mapHeader = ptr->mapID;
+    }else{
+        ptr->x = warpPtr->x;
+        ptr->y = warpPtr->z;
+        MapHeader_GetWorldMapCoords(ptr->mapID, &(s.x), &(s.y));
+        MAPMATRIX* matrix = MapMatrix_New();
+        MapMatrix_Load(0x3c, matrix);
+        if(s.x == 0 && s.y == 0){
+            ptr->MatrixXCoord = ptr->x / 32;
+            ptr->MatrixYCoord = ptr->y /32;
+        }else{
+            ptr->MatrixXCoord = s.x;
+            ptr->MatrixYCoord = s.y;
+        }
+        ptr->mapHeader = MapMatrix_GetMapHeader(matrix, ptr->MatrixXCoord, ptr->MatrixYCoord);
+        MapMatrix_Free(matrix);
+    }
+
+    ptr->Player_Gender = PlayerProfile_GetTrainerGender(Sav2_PlayerData_GetProfileAddr(Fsys_GetSaveDataPtr(sys)));
+    ptr->Unk_ptr28 = (int) sys + 0x10C;
+    ptr->Unk_FsysVar01 = sub_0203DF3C(sys);
+    ptr->Map_MusicID = sub_02054F60(sys, ptr->mapID);
+    
+    if(arg3 != 0)
+        return;
+    if(sub_02092E08(Unk_V1) != 0){
+        ptr->UnkFsysSub_114ptr = ov02_02251EE8(Unk_V1, &(s.Unk_Var0c));
+        if(ptr->UnkFsysSub_114ptr >= 0x4b){
+            ptr->Unk_Bool00 = 0;
+            return;
+        }else{
+            ptr->Unk_Var06 = s.Unk_Var10;
+            ptr->Unk_Var05 = s.Unk_Var0f;
+            if(ptr->Unk_Var05 == 3){
+                sub_0202F050(SaveData_GetPhoneRematches(sys->savedata), s.Unk_Var0e);
+            }
+            else if(ptr->Unk_Var05 == 0){
+                sub_0202AB18(Sav2_Misc_get(sys->savedata), s.Unk_Var0c, s.Unk_Var0d, ptr->UnkFsysSub_114ptr);
+            }
+            sub_02092F64(Unk_V1);
+            ptr->Unk_Bool00 = 1;
+            return;
+    }
+    }else{
+        ptr->Unk_Bool00 = 0;
+        return;
+    }
+}
+
+void sub_02092D80(FieldSystem* sys, UnkSTRUCT_2C* ptr){
+    sub_02092BE8(sys, ptr, 0);
+}
+
+void sub_02092D8C(FieldSystem* sys, UnkSTRUCT_2C* ptr){
+    sub_02092BE8(sys, ptr, 1);
+}
+
+struct UnkFsysSub_114* sub_02092D98(HeapID id, FieldSystem* sys){ 
+    struct UnkFsysSub_114 * ptr = AllocFromHeap(id, sizeof(struct UnkFsysSub_114));
+    MI_CpuFill8(ptr, 0, sizeof(struct UnkFsysSub_114));
+    sub_02092F64(ptr);
+    ptr->Unk_varC = 0xa;
+    ptr->Unk_var10 = 0x1e;
+    ptr->Unk_var12 = 0x32;
+    ptr->data = sys->savedata;
+    ptr->pokegearData = SaveData_GSPlayerMisc_get(ptr->data);
+    ptr->rematchData = SaveData_GetPhoneRematches(ptr->data);
+    ptr->sys = sys;
+    return ptr;
+}
+
+void sub_02092DD8(struct UnkFsysSub_114 * ptr){
+	MI_CpuFill8(ptr, 0, sizeof(struct UnkFsysSub_114));
+	FreeToHeap(ptr);
+}
+
+struct UnkFsysSub_114* sub_02092DEC(FieldSystem* sys){
+    return sys->unk114;
+}
+
+void sub_02092DF4(struct UnkFsysSub_114* ptr){
+	ptr->Unk_var0_0 = 1;
+	ptr->Unk_var14 = 0;
+	ptr->Unk_var18 = 0;
+}
+
+u8 sub_02092E08(struct UnkFsysSub_114* ptr){
+    return ptr->Unk_var0_0;
+}
+
+struct PhoneBookEntry* sub_02092E10(struct UnkFsysSub_114* ptr){
+	return &ptr->entry;
+}
+
+void sub_02092E14(struct UnkFsysSub_114* ptr, u32 Unkarg1, u32 Unkarg2){
+    sub_0202F01C(ptr->rematchData, Unkarg1);
+    if(Unkarg2 != 0){
+        if(ptr->Unk_var8 < (ptr->Unk_varC -1)){
+            ptr->Unk_var8 = (ptr->Unk_varC) -1;
+        }
+    }
+}
