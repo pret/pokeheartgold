8bb56423f4fd060ec0dfc629a944195b76cdc43d
PikalaxALT <pikalaxalt@gmail.com>
2022-03-26 21:01:15 -0400

    phonebook_dat.c

--- include/gear_phone.h
+++ include/gear_phone.h
@@ -1,6 +1,8 @@
 #ifndef POKEHEARTGOLD_GEAR_PHONE_H
 #define POKEHEARTGOLD_GEAR_PHONE_H
 
+#include "heap.h"
+
 struct PhoneBookEntry {
     u8 unk0;
     u8 unk1;
@@ -14,7 +16,7 @@ struct PhoneBookEntry {
 };
 
 struct PhoneBook {
-    u32 count;
+    int count;
     struct PhoneBookEntry * entries;
 };
 
@@ -22,5 +24,11 @@ struct UnkFsysSub_114 {
     u8 filler_00[0x1C];
     struct PhoneBookEntry entry;
 };
+
+struct PhoneBook *AllocAndReadPhoneBook(HeapID heapId);
+void FreePhoneBook(struct PhoneBook *phoneBook);
+u8 LoadPhoneBookEntryI(u16 idx, struct PhoneBookEntry *dest, HeapID heapId);
+int GetPhoneMessageGmm(int idx);
+BOOL sub_02095FF8(int x);
 
 #endif //POKEHEARTGOLD_GEAR_PHONE_H
