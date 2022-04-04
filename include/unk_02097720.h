6da077335474890f66654f71d66eca06d3eab75c
dyedquartz <dyedquartz@gmail.com>
2022-04-04 11:13:47 -0500

    resolved changes

--- include/unk_02097720.h
+++ include/unk_02097720.h
@@ -7,7 +7,7 @@
 typedef struct UnkStruct_02097720 {
     u32 unknown_0;
     u16* unknown_4;
-    UnkStruct_Ov02_0224E4EC* unknown_8; // most likely a struct pointer
+    UnkStruct_Ov02_0224E4EC* unknown_8;
 } UnkStruct_02097720;
 
 void sub_02097720(TaskManager* taskManager, u16* unknownPtr);
