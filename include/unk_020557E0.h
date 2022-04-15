62f253ed4f85bbe838e088459c037d851be5fd5f
Admiral-Fish <admiralfish420@gmail.com>
2022-04-14 22:19:07 -0700

    Decomp scrcmd_12

---
+++ include/unk_020557E0.h
@@ -0,0 +1,15 @@
+#ifndef POKEHEARTGOLD_UNK_020557E0_H
+#define POKEHEARTGOLD_UNK_020557E0_H
+
+#include "script.h"
+#include "unk_0202EB30.h"
+
+void sub_020557E0(FieldSystem *fsys);
+void sub_020557F8(FieldSystem *fsys);
+void sub_02055828(FieldSystem *fsys);
+void sub_0202EC70(struct UnkStruct_0202EB30 *a0, int i, POKEMON *pokemon);
+int sub_020558D0(FieldSystem *fsys);
+int sub_020558E0(FieldSystem *fsys);
+int sub_020558F0(FieldSystem *fsys);
+
+#endif //POKEHEARTGOLD_UNK_020557E0_H
