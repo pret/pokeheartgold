9d64924be37fa7a921f843145c6c2092cced1850
PikalaxALT <pikalaxalt@gmail.com>
2022-04-22 08:29:37 -0400

    gave up on ov04_02255140

--- src/blackthorn_gymmick.c
+++ src/blackthorn_gymmick.c
@@ -10,10 +10,12 @@ struct Vec2Fx32 {
 };
 
 struct BlackthornGymPlatform {
-    u8 filler_000[0x7];
+    int unk_000;
+    u8 unk_004;
+    u8 unk_005;
+    u8 unk_006;
     u8 modelId;
-    u8 filler_008[0x1D8];
-    struct Vec2Fx32 unk_1E0[18];
+    struct Vec2Fx32 unk_008[77];
 }; // size=0x270
 
 struct BlackthornGymWork {
@@ -22,6 +24,7 @@ struct BlackthornGymWork {
 }; // size=0x754
 
 void ov04_02255140(u8 idx, u8 rot, u16 x, u16 z, struct BlackthornGymPlatform *platform);
+void ov04_022554FC(u8 rot, u16 x, u16 z, struct BlackthornGymPlatform *platform);
 BOOL ov04_02255708(struct BlackthornGymWork *gymWork, u16 x, u16 z);
 
 /*
@@ -95,7 +98,7 @@ BOOL ov04_022550D4(FieldSystem* fsys, fx32 x, fx32 z, u32 a3, BOOL *p_ret) {
     for (i = 0; i < 3; i++) {
         struct BlackthornGymPlatform *platform = &gymWork->platforms[i];
         for (j = 0; j < 18; j++) {
-            if (x == platform->unk_1E0[j].x && z == platform->unk_1E0[j].z) {
+            if (x == platform->unk_008[j + 59].x && z == platform->unk_008[j + 59].z) {
                 *p_ret = FALSE;
                 return TRUE;
             }
@@ -106,4 +109,485 @@ BOOL ov04_022550D4(FieldSystem* fsys, fx32 x, fx32 z, u32 a3, BOOL *p_ret) {
         return TRUE;
     }
     return FALSE;
+}
+
+asm // Someone please tell me what the h*ck is going on here
+void ov04_02255140(u8 idx, u8 rot, u16 x, u16 z, struct BlackthornGymPlatform *platform) {
+ /*   int i, j;
+    // sp0 = rot
+    // sp4 = x
+    // sp8 = z
+    // sp38 = platform
+
+    platform->unk_008[0].x = x;
+    platform->unk_008[0].z = z;
+    platform->unk_008[1].x = x + 1;
+    platform->unk_008[1].z = z;
+    platform->unk_008[2].x = x - 1;
+    platform->unk_008[2].z = z;
+    platform->unk_008[3].x = x;
+    platform->unk_008[3].z = z;
+
+    if (idx != 1) {
+        platform->unk_000 = 0;
+        platform->unk_004 = 5;
+        platform->unk_005 = 7;
+        for (i = 0; i < 7; i++) {
+            platform->unk_008[4 + i].x = x + 3;
+            platform->unk_008[4 + i].z = (z - 2) + i;
+        }
+        for (i = 0; i < 7; i++) {
+            platform->unk_008[12 + i].x = x - 3;
+            platform->unk_008[12 + i].z = (z - 2) + i;
+        }
+        for (i = 0; i < 5; i++) {
+            platform->unk_008[20 + i].x = x - 2;
+            platform->unk_008[20 + i].z = (z + 5) + i;
+        }
+        for (i = 0; i < 5; i++) {
+            platform->unk_008[25 + i].x = x - 2;
+            platform->unk_008[25 + i].z = (z - 3) + i;
+        }
+        for (i = 0; i < 3; i++) {
+            for (j = 0; j < 7; j++) {
+                platform->unk_008[30 + i * 7 + j].x = (x - 4 - i);
+                platform->unk_008[30 + i * 7 + j].z = (z + 4) - j;
+            }
+        }
+    }*/
+
+    push {r4, r5, r6, r7, lr}
+    sub sp, #0x24
+    str r1, [sp]
+    str r2, [sp, #4]
+    str r3, [sp, #8]
+    ldr r1, [sp, #0x38]
+    ldr r2, [sp, #4]
+    str r1, [sp, #0x38]
+    str r2, [r1, #8]
+    ldr r2, [sp, #8]
+    str r2, [r1, #0xc]
+    ldr r1, [sp, #4]
+    add r2, r1, #1
+    ldr r1, [sp, #0x38]
+    str r2, [r1, #0x10]
+    ldr r2, [sp, #8]
+    str r2, [r1, #0x14]
+    ldr r1, [sp, #4]
+    sub r7, r1, #1
+    ldr r1, [sp, #0x38]
+    cmp r0, #1
+    str r7, [r1, #0x18]
+    ldr r2, [sp, #8]
+    str r2, [r1, #0x1c]
+    ldr r2, [sp, #4]
+    str r2, [r1, #0x20]
+    ldr r2, [sp, #8]
+    str r2, [r1, #0x24]
+    bne @_0225517C
+    b @_022552D6
+@_0225517C:
+    mov r0, #0
+    str r0, [r1]
+    mov r2, #5
+    strb r2, [r1, #4]
+    mov r2, #7
+    strb r2, [r1, #5]
+    ldr r1, [sp, #4]
+    ldr r4, [sp, #0x38]
+    str r1, [sp, #0x1c]
+    ldr r1, [sp, #8]
+    str r1, [sp, #0x20]
+    ldr r1, [sp, #4]
+    add r3, r1, #3
+    ldr r1, [sp, #8]
+    str r3, [sp, #0x1c]
+    sub r2, r1, #2
+    str r2, [sp, #0x20]
+@_0225519E:
+    add r1, r2, r0
+    str r3, [r4, #0x28]
+    str r1, [r4, #0x2c]
+    add r0, r0, #1
+    add r4, #8
+    cmp r0, #7
+    blt @_0225519E
+    ldr r0, [sp, #4]
+    ldr r3, [sp, #0x38]
+    str r0, [sp, #0x1c]
+    ldr r0, [sp, #8]
+    mov r4, #0
+    str r0, [sp, #0x20]
+    ldr r0, [sp, #4]
+    sub r2, r0, #3
+    ldr r0, [sp, #8]
+    str r2, [sp, #0x1c]
+    sub r1, r0, #2
+    str r1, [sp, #0x20]
+@_022551C4:
+    add r0, r1, r4
+    str r2, [r3, #0x68]
+    str r0, [r3, #0x6c]
+    add r4, r4, #1
+    add r3, #8
+    cmp r4, #7
+    blt @_022551C4
+    ldr r0, [sp, #4]
+    ldr r2, [sp, #0x38]
+    str r0, [sp, #0x1c]
+    ldr r0, [sp, #8]
+    mov r3, #0
+    str r0, [sp, #0x20]
+    ldr r0, [sp, #4]
+    sub r1, r0, #2
+    ldr r0, [sp, #8]
+    str r1, [sp, #0x1c]
+    add r0, r0, #5
+    str r0, [sp, #0x20]
+@_022551EA:
+    add r4, r2, #0
+    add r4, #0xa8
+    str r1, [r4]
+    add r4, r2, #0
+    add r4, #0xac
+    str r0, [r4]
+    add r4, r2, #0
+    add r4, #0xa8
+    ldr r4, [r4, #0]
+    add r5, r4, r3
+    add r4, r2, #0
+    add r4, #0xa8
+    add r3, r3, #1
+    add r2, #8
+    str r5, [r4]
+    cmp r3, #5
+    blt @_022551EA
+    ldr r0, [sp, #4]
+    ldr r2, [sp, #0x38]
+    str r0, [sp, #0x1c]
+    ldr r0, [sp, #8]
+    mov r3, #0
+    str r0, [sp, #0x20]
+    ldr r0, [sp, #4]
+    sub r1, r0, #2
+    ldr r0, [sp, #8]
+    str r1, [sp, #0x1c]
+    sub r0, r0, #3
+    str r0, [sp, #0x20]
+@_02255224:
+    add r4, r2, #0
+    add r4, #0xd0
+    str r1, [r4]
+    add r4, r2, #0
+    add r4, #0xd4
+    str r0, [r4]
+    add r4, r2, #0
+    add r4, #0xd0
+    ldr r4, [r4, #0]
+    add r5, r4, r3
+    add r4, r2, #0
+    add r4, #0xd0
+    add r3, r3, #1
+    add r2, #8
+    str r5, [r4]
+    cmp r3, #5
+    blt @_02255224
+    ldr r0, [sp, #4]
+    mov r6, #0
+    sub r0, r0, #4
+    str r0, [sp, #0xc]
+    ldr r0, [sp, #8]
+    str r6, [sp, #0x10]
+    add r4, r0, #4
+@_02255254:
+    ldr r0, [sp, #0xc]
+    str r4, [sp, #0x20]
+    str r0, [sp, #0x1c]
+    sub r2, r0, r6
+    ldr r0, [sp, #0x10]
+    str r2, [sp, #0x1c]
+    lsl r1, r0, #3
+    ldr r0, [sp, #0x38]
+    mov r3, #0
+    add r5, r0, r1
+@_02255268:
+    add r0, r5, #0
+    add r0, #0xf8
+    str r2, [r0]
+    add r0, r5, #0
+    add r0, #0xfc
+    str r4, [r0]
+    add r0, r5, #0
+    add r0, #0xfc
+    ldr r0, [r0, #0]
+    sub r1, r0, r3
+    add r0, r5, #0
+    add r0, #0xfc
+    add r3, r3, #1
+    add r5, #8
+    str r1, [r0]
+    cmp r3, #7
+    blt @_02255268
+    ldr r0, [sp, #0x10]
+    add r6, r6, #1
+    add r0, r0, #7
+    str r0, [sp, #0x10]
+    cmp r6, #2
+    blt @_02255254
+    ldr r0, [sp, #0x38]
+    mov r4, #0
+    ldrb r3, [r0, #4]
+    cmp r3, #0
+    ble @_022552D4
+    ldr r0, [sp, #4]
+    add r2, r0, #3
+    ldr r0, [sp, #8]
+    str r2, [sp, #0x1c]
+    sub r1, r0, #2
+    str r1, [sp, #0x20]
+    cmp r3, #0
+    ble @_022552D4
+    mov r6, #0x6f
+    lsl r6, r6, #2
+    ldr r3, [sp, #0x38]
+    add r0, r6, #0
+@_022552B8:
+    mov r5, #0x6e
+    lsl r5, r5, #2
+    str r2, [r3, r5]
+    add r5, r5, #4
+    str r1, [r3, r5]
+    ldr r5, [r3, r6]
+    add r5, r5, r4
+    str r5, [r3, r0]
+    ldr r5, [sp, #0x38]
+    add r4, r4, #1
+    ldrb r5, [r5, #4]
+    add r3, #8
+    cmp r4, r5
+    blt @_022552B8
+@_022552D4:
+    b @_02255432
+@_022552D6:
+    ldr r0, [sp, #0x38]
+    mov r1, #1
+    str r1, [r0]
+    mov r1, #4
+    strb r1, [r0, #4]
+    mov r1, #8
+    strb r1, [r0, #5]
+    ldr r0, [sp, #4]
+    ldr r4, [sp, #0x38]
+    str r0, [sp, #0x1c]
+    ldr r0, [sp, #8]
+    mov r3, #0
+    str r0, [sp, #0x20]
+    ldr r0, [sp, #4]
+    add r2, r0, #2
+    ldr r0, [sp, #8]
+    str r2, [sp, #0x1c]
+    sub r1, r0, #2
+    str r1, [sp, #0x20]
+@_022552FC:
+    add r0, r1, r3
+    str r2, [r4, #0x28]
+    str r0, [r4, #0x2c]
+    add r3, r3, #1
+    add r4, #8
+    cmp r3, #8
+    blt @_022552FC
+    ldr r0, [sp, #4]
+    ldr r3, [sp, #0x38]
+    str r0, [sp, #0x1c]
+    ldr r0, [sp, #8]
+    mov r4, #0
+    str r0, [sp, #0x20]
+    ldr r0, [sp, #4]
+    sub r2, r0, #3
+    ldr r0, [sp, #8]
+    str r2, [sp, #0x1c]
+    sub r1, r0, #2
+    str r1, [sp, #0x20]
+@_02255322:
+    add r0, r1, r4
+    str r2, [r3, #0x68]
+    str r0, [r3, #0x6c]
+    add r4, r4, #1
+    add r3, #8
+    cmp r4, #8
+    blt @_02255322
+    ldr r0, [sp, #4]
+    ldr r2, [sp, #0x38]
+    str r0, [sp, #0x1c]
+    ldr r0, [sp, #8]
+    mov r3, #0
+    str r0, [sp, #0x20]
+    ldr r0, [sp, #4]
+    sub r1, r0, #2
+    ldr r0, [sp, #8]
+    str r1, [sp, #0x1c]
+    add r0, r0, #6
+    str r0, [sp, #0x20]
+@_02255348:
+    add r4, r2, #0
+    add r4, #0xa8
+    str r1, [r4]
+    add r4, r2, #0
+    add r4, #0xac
+    str r0, [r4]
+    add r4, r2, #0
+    add r4, #0xa8
+    ldr r4, [r4, #0]
+    add r5, r4, r3
+    add r4, r2, #0
+    add r4, #0xa8
+    add r3, r3, #1
+    add r2, #8
+    str r5, [r4]
+    cmp r3, #4
+    blt @_02255348
+    ldr r0, [sp, #4]
+    ldr r2, [sp, #0x38]
+    str r0, [sp, #0x1c]
+    ldr r0, [sp, #8]
+    mov r3, #0
+    str r0, [sp, #0x20]
+    ldr r0, [sp, #4]
+    sub r1, r0, #2
+    ldr r0, [sp, #8]
+    str r1, [sp, #0x1c]
+    sub r0, r0, #3
+    str r0, [sp, #0x20]
+@_02255382:
+    add r4, r2, #0
+    add r4, #0xd0
+    str r1, [r4]
+    add r4, r2, #0
+    add r4, #0xd4
+    str r0, [r4]
+    add r4, r2, #0
+    add r4, #0xd0
+    ldr r4, [r4, #0]
+    add r5, r4, r3
+    add r4, r2, #0
+    add r4, #0xd0
+    add r3, r3, #1
+    add r2, #8
+    str r5, [r4]
+    cmp r3, #4
+    blt @_02255382
+    ldr r0, [sp, #4]
+    mov r6, #0
+    sub r0, r0, #4
+    str r0, [sp, #0x14]
+    ldr r0, [sp, #8]
+    str r6, [sp, #0x18]
+    add r4, r0, #5
+@_022553B2:
+    ldr r0, [sp, #0x14]
+    str r4, [sp, #0x20]
+    str r0, [sp, #0x1c]
+    sub r2, r0, r6
+    ldr r0, [sp, #0x18]
+    str r2, [sp, #0x1c]
+    lsl r1, r0, #3
+    ldr r0, [sp, #0x38]
+    mov r3, #0
+    add r5, r0, r1
+@_022553C6:
+    add r0, r5, #0
+    add r0, #0xf8
+    str r2, [r0]
+    add r0, r5, #0
+    add r0, #0xfc
+    str r4, [r0]
+    add r0, r5, #0
+    add r0, #0xfc
+    ldr r0, [r0, #0]
+    sub r1, r0, r3
+    add r0, r5, #0
+    add r0, #0xfc
+    add r3, r3, #1
+    add r5, #8
+    str r1, [r0]
+    cmp r3, #8
+    blt @_022553C6
+    ldr r0, [sp, #0x18]
+    add r6, r6, #1
+    add r0, #8
+    str r0, [sp, #0x18]
+    cmp r6, #3
+    blt @_022553B2
+    ldr r0, [sp, #0x38]
+    mov r4, #0
+    ldrb r3, [r0, #4]
+    cmp r3, #0
+    ble @_02255432
+    ldr r0, [sp, #4]
+    add r2, r0, #3
+    ldr r0, [sp, #8]
+    str r2, [sp, #0x1c]
+    sub r1, r0, #2
+    str r1, [sp, #0x20]
+    cmp r3, #0
+    ble @_02255432
+    mov r6, #0x6f
+    lsl r6, r6, #2
+    ldr r3, [sp, #0x38]
+    add r0, r6, #0
+@_02255416:
+    mov r5, #0x6e
+    lsl r5, r5, #2
+    str r2, [r3, r5]
+    add r5, r5, #4
+    str r1, [r3, r5]
+    ldr r5, [r3, r6]
+    add r5, r5, r4
+    str r5, [r3, r0]
+    ldr r5, [sp, #0x38]
+    add r4, r4, #1
+    ldrb r5, [r5, #4]
+    add r3, #8
+    cmp r4, r5
+    blt @_02255416
+@_02255432:
+    ldr r0, [sp, #8]
+    mov r6, #0x1e
+    sub r2, r0, #1
+    mov r1, #0
+    lsl r6, r6, #4
+    str r7, [sp, #0x1c]
+    str r2, [sp, #0x20]
+    mov ip, r1
+    add r4, r6, #4
+@_02255444:
+    ldr r3, [sp, #0x38]
+    lsl r5, r1, #3
+    mov r0, #0
+    add r5, r3, r5
+@_0225544C:
+    add r3, r7, r0
+    str r3, [r5, r6]
+    str r2, [r5, r4]
+    add r0, r0, #1
+    add r5, #8
+    cmp r0, #3
+    blt @_0225544C
+    mov r0, ip
+    add r0, r0, #1
+    add r1, r1, #3
+    add r2, r2, #1
+    mov ip, r0
+    cmp r0, #6
+    blt @_02255444
+    ldr r1, [sp, #0]
+    ldr r0, [sp, #0x38]
+    ldr r3, [sp, #0x38]
+    strb r1, [r0, #6]
+    ldr r0, [sp, #0]
+    ldr r1, [sp, #4]
+    ldr r2, [sp, #8]
+    bl ov04_022554FC
+    add sp, #0x24
+    pop {r4, r5, r6, r7, pc}
 }
