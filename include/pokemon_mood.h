407155551f28e2b8ecd3c80e5d683d54379585a5
Adrienn Tindall <33725376+aidenfrog@users.noreply.github.com>
2022-09-03 01:56:56 -0400

    Fix build errors caused by rename

--- include/pokemon_mood.h
+++ include/pokemon_mood.h
@@ -1,6 +1,8 @@
 #ifndef POKEHEARTGOLD_POKEMON_MOOD_H
 #define POKEHEARTGOLD_POKEMON_MOOD_H
 
+#include "pokemon.h"
+
 void ApplyItemEffectOnMonMood(POKEMON *pokemon, u16 itemId);
 void ApplyMonMoodModifier(POKEMON *pokemon, u32 modifierId);
 
--- src/script_pokemon_util.c
+++ src/script_pokemon_util.c
@@ -1,7 +1,7 @@
 #include "party.h"
 #include "unk_0202ECC0.h"
 #include "unk_0206979C.h"
-#include "unk_02097E4C.h"
+#include "pokemon_mood.h"
 #include "script_pokemon_util.h"
 #include "constants/items.h"
 
