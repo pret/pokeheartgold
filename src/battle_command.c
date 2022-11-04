5b4b88e419b697beb5a9e453b8c91d20377a0217
Adrienn Tindall <33725376+aidenfrog@users.noreply.github.com>
2022-11-03 23:57:02 -0400

    Battle Command, 3

--- src/battle_command.c
+++ src/battle_command.c
@@ -459,3 +459,229 @@ BOOL ov12_0223D700(BattleSystem *bsys, BATTLECONTEXT *ctx) {
     return FALSE;
 }
 
+BOOL ov12_0223D8B0(BattleSystem *bsys, BATTLECONTEXT *ctx) {
+    int i;
+    int unkA = ov12_0223A7F0(bsys);
+    OpponentData *opponentData;
+
+    BattleScriptIncrementPointer(ctx, 1);
+
+    u32 unkB = BattleScriptReadWord(ctx);
+    u32 unkC = BattleScriptReadWord(ctx);
+
+    switch (unkB) {
+    case 0:
+    default:
+        for (i = 0; i < unkA; i++) {
+            if ((ov12_0223A7E0(bsys) & 2) && (i > 1)) {
+                break;
+            }
+            ov12_022629F8(bsys, i, unkC);
+        }
+        break;
+    case 3:
+        for (i = 0; i < unkA; i++) {
+            opponentData = ov12_0223A7E8(bsys, i);
+            if ((opponentData->unk195 & 1) == FALSE) {
+                ov12_022629F8(bsys, i, unkC);
+                if ((ov12_0223A7E0(bsys) & 2)) {
+                    break;
+                }
+            }
+        }
+        break;
+    case 4:
+        for (i = 0; i < unkA; i++) {
+            opponentData = ov12_0223A7E8(bsys, i);
+            if (opponentData->unk195 & 1) {
+                ov12_022629F8(bsys, i, unkC);
+                if ((ov12_0223A7E0(bsys) & 2)) {
+                    break;
+                }
+            }
+        }
+        break;
+    case 9:
+        for (i = 0; i < unkA; i++) {
+            opponentData = ov12_0223A7E8(bsys, i);
+            if (opponentData->unk195 == 0 || opponentData->unk195 == 2) {
+                ov12_022629F8(bsys, i, unkC);
+                break;
+            }
+        }
+        break;
+    case 10:
+        for (i = 0; i < unkA; i++) {
+            opponentData = ov12_0223A7E8(bsys, i);
+            if (opponentData->unk195 == 1 || opponentData->unk195 == 3) {
+                ov12_022629F8(bsys, i, unkC);
+                break;
+            }
+        }
+        break;
+    case 11:
+        for (i = 0; i < unkA; i++) {
+            opponentData = ov12_0223A7E8(bsys, i);
+            if (opponentData->unk195 == 4) {
+                ov12_022629F8(bsys, i, unkC);
+                break;
+            }
+        }
+        break;
+    case 12:
+        for (i = 0; i < unkA; i++) {
+            opponentData = ov12_0223A7E8(bsys, i);
+            if (opponentData->unk195 == 5) {
+                ov12_022629F8(bsys, i, unkC);
+                break;
+            }
+        }
+        break;
+    }
+    
+    return FALSE;
+}
+
+BOOL ov12_0223DA48(BattleSystem *bsys, BATTLECONTEXT *ctx) {
+    int i;
+    int unkA = ov12_0223A7F0(bsys);
+    BattleScriptIncrementPointer(ctx, 1);
+
+    for (i = 0; i < unkA; i++) {
+        //TODO: Declare
+        ov12_02263CB0(bsys, i);
+    }
+
+    return FALSE;
+}
+
+BOOL ov12_0223DA74(BattleSystem *bsys, BATTLECONTEXT *ctx) {
+    int i;
+    int unkA = ov12_0223A7F0(bsys);
+    OpponentData *opponentData;
+
+    BattleScriptIncrementPointer(ctx, 1);
+    int unkB = BattleScriptReadWord(ctx);
+
+    switch (unkB) {
+    case 0:
+        for (i = 0; i < unkA; i++) {
+            ov12_02262A2C(bsys, ctx, i, 0);
+        }
+        break;
+    case 3:
+        for (i = 0; i < unkA; i++) {
+            opponentData = ov12_0223A7E8(bsys, i);
+            if ((opponentData->unk195 & 1) == 0) {
+                ov12_02262A2C(bsys, ctx, i, 0);
+            }
+        }
+        break;
+    case 4:
+        for (i = 0; i < unkA; i++) {
+            opponentData = ov12_0223A7E8(bsys, i);
+            if (opponentData->unk195 & 1) {
+                ov12_02262A2C(bsys, ctx, i, 0);
+            }
+        }
+        break;
+    default:
+        ov12_02262A2C(bsys, ctx, ov12_0224768C(bsys, ctx, unkB), 0);
+        break;
+    }
+
+    return FALSE;
+}
+
+BOOL ov12_0223DB2C(BattleSystem *bsys, BATTLECONTEXT *ctx) {
+    int i;
+    int unkA = ov12_0223A7F0(bsys);
+    OpponentData *opponentData;
+    u8 delay;
+
+    BattleScriptIncrementPointer(ctx, 1);
+    int unkB = BattleScriptReadWord(ctx);
+
+    delay = 0;
+
+    switch (unkB) {
+    case 0:
+        for (i = 0; i < unkA; i++) {
+            ov12_02262A2C(bsys, ctx, i, 0);
+        }
+        break;
+    case 3:
+        for (i = 0; i < unkA; i++) {
+            opponentData = ov12_0223A7E8(bsys, i);
+            if ((opponentData->unk195 & 1) == 0) {
+                ov12_02262A2C(bsys, ctx, i, delay);
+                delay += 4;
+            }
+        }
+        break;
+    case 4:
+        for (i = 0; i < unkA; i++) {
+            opponentData = ov12_0223A7E8(bsys, i);
+            if (opponentData->unk195 & 1) {
+                ov12_02262A2C(bsys, ctx, i, delay);
+                delay += 4;
+            }
+        }
+        break;
+    default:
+        ov12_02262A2C(bsys, ctx, ov12_0224768C(bsys, ctx, unkB), 0);
+        break;
+    }
+    
+    return FALSE;
+}
+
+BOOL ov12_0223DBF4(BattleSystem *bsys, BATTLECONTEXT *ctx) {
+    int i;
+    int unkA = ov12_0223A7F0(bsys);
+    OpponentData *opponentData;
+
+    BattleScriptIncrementPointer(ctx, 1);
+    int unkB = BattleScriptReadWord(ctx);
+
+    switch (unkB) {
+    case 0:
+        for (i = 0; i < unkA; i++) {
+            ov12_02262B64(bsys, i);
+        }
+        break;
+    case 3:
+        for (i = 0; i < unkA; i++) {
+            opponentData = ov12_0223A7E8(bsys, i);
+            if ((opponentData->unk195 & 1) == 0 && (ctx->unk_3108 & MaskOfFlagNo(i)) == 0) {
+                ov12_02262B64(bsys, i);
+            }
+        }
+        break;
+    case 4:
+        for (i = 0; i < unkA; i++) {
+            opponentData = ov12_0223A7E8(bsys, i);
+            if (opponentData->unk195 & 1) {
+                ov12_02262B64(bsys, i);
+            }
+        }
+        break;
+    default:
+        ov12_02262B64(bsys, ov12_0224768C(bsys, ctx, unkB));
+        break;
+    }
+    
+    return FALSE;
+}
+
+BOOL ov12_0223DCAC(BattleSystem *bsys, BATTLECONTEXT *ctx) {
+    if (ov12_0224ED48(ctx)) {
+        BattleScriptIncrementPointer(ctx, 1);
+    } else {
+        ov12_0224ED9C(ctx);
+    }
+
+    ctx->unk_3154 = 1;
+
+    return FALSE;
+}
