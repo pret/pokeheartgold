1229024cc380b9d92bad1b432bafabe2588e85c4
Akashi Akira <rubenru09@aol.com>
2023-05-24 21:15:21 +0100

    fix some warnings

--- include/unk_02078E30.h
+++ include/unk_02078E30.h
@@ -6,5 +6,9 @@
 extern struct OVY_MGR_TEMPLATE _0210159C;
 
 void sub_0207991C(PartyMenuStruct *unkPtr, int a1);
+u32 sub_0207CA9C(void);
+u32 sub_0207CAA0(void);
+u32 sub_0207CAA4(void);
+u32 sub_0207CAA8(void);
 
 #endif //POKEHEARTGOLD_UNK_02078E30_H
