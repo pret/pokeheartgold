796ac1fa2413f98dd124c5b63d792c82d367dc43
Laquin-Laquih <anderaginaga2@gmail.com>
2023-06-01 22:25:06 +0200

    rename parameters
    I skipped these ones, oops

--- include/message_printer.h
+++ include/message_printer.h
@@ -8,7 +8,7 @@ typedef struct MessagePrinter {
     u32 color;
 } MessagePrinter;
 
-MessagePrinter *MessagePrinter_new(u32 color1, u32 color2, u32 color3, HeapID heap_id);
-void MessagePrinter_delete(MessagePrinter *a0);
+MessagePrinter *MessagePrinter_new(u32 color1, u32 color2, u32 color3, HeapID heapId);
+void MessagePrinter_delete(MessagePrinter *messagePrinter);
 void sub_0200CDAC(MessagePrinter *messagePrinter, u8 a1, WINDOW *window, u32 x, u32 y);
 void PrintUIntOnWindow(MessagePrinter *messagePrinter, u32 num, u32 ndigits, STRCONVMODE mode, WINDOW *window, u32 x, u32 y);
