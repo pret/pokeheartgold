b1f094c75036e37898af833f9bdd440962faa145
pikalaxalt <pikalaxalt@users.noreply.github.com>
2023-12-09 14:58:32 -0500

    MGGive_Mon

--- src/scrcmd_11.c
+++ src/scrcmd_11.c
@@ -1,7 +1,13 @@
 #include "global.h"
 #include "mystery_gift.h"
 #include "scrcmd.h"
+#include "math_util.h"
+#include "save_vars_flags.h"
 #include "script_pokemon_util.h"
+#include "ribbon.h"
+#include "pokedex.h"
+#include "sys_vars.h"
+#include "trainer_memo.h"
 #include "constants/map_sections.h"
 #include "msgdata/msg.naix"
 #include "msgdata/msg/msg_0209.h"
@@ -177,16 +183,131 @@ BOOL ScrCmd_MysteryGift(ScriptContext* ctx) {
     return FALSE;
 }
 
-BOOL MGCheck_PartySpace(FieldSystem* fieldSystem, MysteryGiftData* mgData) {
-    return Party_GetCount(SaveArray_Party_Get(fieldSystem->saveData)) < PARTY_SIZE;
+BOOL MGCheck_PartySpace(FieldSystem* fieldSys, MysteryGiftData* unused) {
+    (void)unused;
+    return Party_GetCount(SaveArray_Party_Get(fieldSys->saveData)) < PARTY_SIZE;
 }
 
-void MGGive_ManaphyEgg(FieldSystem* fieldSystem, MysteryGiftData* mgData) {
-    GiveEgg(HEAP_ID_32, fieldSystem->saveData, SPECIES_MANAPHY, MAPSEC_TWINLEAF_TOWN, MAPSECTYPE_EXTERNAL, MAPLOC(METLOC_POKEMON_RANGER));
+void MGGive_ManaphyEgg(FieldSystem* fieldSys, MysteryGiftData* unused) {
+    (void)unused;
+    GiveEgg(HEAP_ID_32, fieldSys->saveData, SPECIES_MANAPHY, MAPSEC_TWINLEAF_TOWN, MAPSECTYPE_EXTERNAL, MAPLOC(METLOC_POKEMON_RANGER));
 }
 
 void MGMessageSuccess_ManaphyEgg(struct GetMysteryGiftGmmState* gmmState, u16* pMsgBank, u16* pMsgNum) {
     *pMsgBank = NARC_msg_msg_0209_bin;
     *pMsgNum = msg_0209_00013;
     BufferPlayersName(gmmState->msgFormat, 0, Save_PlayerData_GetProfileAddr(gmmState->fieldSys->saveData));
+}
+
+void MGGive_Mon(FieldSystem* fieldSys, MysteryGiftData* unused) {
+    (void)unused;
+
+    MG_POKEMON_TAG* mgData = &FieldSystem_GetDataOfNextMG(fieldSys)->pokemon;
+    PlayerProfile* profile = Save_PlayerData_GetProfileAddr(fieldSys->saveData);
+    SaveVarsFlags* vars_flags = Save_VarsFlags_Get(fieldSys->saveData);
+    Pokemon* tmpPokemon = NULL;
+    Pokemon* pokemon = &mgData->mon;
+    u8* srcRibbons = mgData->ribbons;
+    int eggMetLocation = GetMonData(pokemon, MON_DATA_EGG_MET_LOCATION, NULL);
+    int personality = GetMonData(pokemon, MON_DATA_PERSONALITY, NULL);
+    int otid = GetMonData(pokemon, MON_DATA_OTID, NULL);
+    int rand = PRandom(OS_GetTick());
+
+    if (personality != 0) {
+        if (personality == 1) {
+            while (CalcShininessByOtIdAndPersonality(otid, rand)) {
+                rand = PRandom(rand);
+            }
+        } else {
+            rand = personality;
+        }
+    }
+    SetMonPersonality(pokemon, rand);
+
+    int gender = GetMonGender(pokemon);
+    SetMonData(pokemon, MON_DATA_GENDER, &gender);
+
+    gender = GetMonData(pokemon, MON_DATA_HP_IV, NULL)
+        + GetMonData(pokemon, MON_DATA_ATK_IV, NULL)
+        + GetMonData(pokemon, MON_DATA_DEF_IV, NULL)
+        + GetMonData(pokemon, MON_DATA_SPEED_IV, NULL)
+        + GetMonData(pokemon, MON_DATA_SPATK_IV, NULL)
+        + GetMonData(pokemon, MON_DATA_SPDEF_IV, NULL);
+    if (gender == 0) {
+        u16 ivRand = LCRandom();
+        rand = ivRand & 0x1Fu;
+        SetMonData(pokemon, MON_DATA_HP_IV, &rand);
+        rand = (ivRand & 0x3E0u) >> 5;
+        SetMonData(pokemon, MON_DATA_ATK_IV, &rand);
+        rand = (ivRand & 0x7C00u) >> 10;
+        SetMonData(pokemon, MON_DATA_DEF_IV, &rand);
+        ivRand = LCRandom();
+        rand = ivRand & 0x1Fu;
+        SetMonData(pokemon, MON_DATA_SPEED_IV, &rand);
+        rand = (ivRand & 0x3E0u) >> 5;
+        SetMonData(pokemon, MON_DATA_SPATK_IV, &rand);
+        rand = (ivRand & 0x7C00u) >> 10;
+        SetMonData(pokemon, MON_DATA_SPDEF_IV, &rand);
+    }
+
+    SaveSpecialRibbons* ribbons = Save_SpecialRibbons_Get(fieldSys->saveData);
+    if (GetMonData(pokemon, MON_DATA_RED_RIBBON, NULL)) {
+        ribbons->ribbons[GetSpecialRibbonNo(RIBBON_RED)] = srcRibbons[0];
+    }
+    if (GetMonData(pokemon, MON_DATA_GREEN_RIBBON, NULL)) {
+        ribbons->ribbons[GetSpecialRibbonNo(RIBBON_GREEN)] = srcRibbons[1];
+    }
+    if (GetMonData(pokemon, MON_DATA_BLUE_RIBBON, NULL)) {
+        ribbons->ribbons[GetSpecialRibbonNo(RIBBON_BLUE)] = srcRibbons[2];
+    }
+    if (GetMonData(pokemon, MON_DATA_FESTIVAL_RIBBON, NULL)) {
+        ribbons->ribbons[GetSpecialRibbonNo(RIBBON_FESTIVAL)] = srcRibbons[3];
+    }
+    if (GetMonData(pokemon, MON_DATA_CARNIVAL_RIBBON, NULL)) {
+        ribbons->ribbons[GetSpecialRibbonNo(RIBBON_CARNIVAL)] = srcRibbons[4];
+    }
+    if (GetMonData(pokemon, MON_DATA_CLASSIC_RIBBON, NULL)) {
+        ribbons->ribbons[GetSpecialRibbonNo(RIBBON_CLASSIC)] = srcRibbons[5];
+    }
+    if (GetMonData(pokemon, MON_DATA_PREMIER_RIBBON, NULL)) {
+        ribbons->ribbons[GetSpecialRibbonNo(RIBBON_PREMIER)] = srcRibbons[6];
+    }
+    if (GetMonData(pokemon, MON_DATA_HOENN_MARINE_RIBBON, NULL)) {
+        ribbons->ribbons[GetSpecialRibbonNo(RIBBON_HOENN_MARINE)] = srcRibbons[7];
+    }
+    if (GetMonData(pokemon, MON_DATA_HOENN_LAND_RIBBON, NULL)) {
+        ribbons->ribbons[GetSpecialRibbonNo(RIBBON_HOENN_LAND)] = srcRibbons[8];
+    }
+    if (GetMonData(pokemon, MON_DATA_HOENN_SKY_RIBBON, NULL)) {
+        ribbons->ribbons[GetSpecialRibbonNo(RIBBON_HOENN_SKY)] = srcRibbons[9];
+    }
+
+    if (mgData->fixedOT == OT_ID_PLAYER_ID) {
+        String* playerName = PlayerProfile_GetPlayerName_NewString(profile, HEAP_ID_32);
+        u32 trainerId = PlayerProfile_GetTrainerID(profile);
+        BOOL gender = PlayerProfile_GetTrainerGender(profile);
+
+        tmpPokemon = AllocMonZeroed(HEAP_ID_32);
+        #ifdef UBFIX
+        GF_ASSERT(pokemon != NULL);
+        #endif
+        CopyPokemonToPokemon(pokemon, tmpPokemon);
+        SetMonData(tmpPokemon, MON_DATA_OT_NAME_2, playerName);
+        SetMonData(tmpPokemon, MON_DATA_OTID, &trainerId);
+        SetMonData(tmpPokemon, MON_DATA_MET_GENDER, &gender);
+        pokemon = tmpPokemon;
+        String_Delete(playerName);
+    }
+
+    MonSetTrainerMemo(pokemon, profile, 4, sub_02017FE4(MAPSECTYPE_EXTERNAL, eggMetLocation), HEAP_ID_32);
+    if (GetMonData(pokemon, MON_DATA_SPECIES, NULL) == SPECIES_ARCEUS && GetMonData(pokemon, MON_DATA_FATEFUL_ENCOUNTER, NULL) == TRUE && !Save_VarsFlags_GetVar404C(vars_flags)) {
+        Save_VarsFlags_SetVar404C(vars_flags, TRUE);
+    }
+    CalcMonLevelAndStats(pokemon);
+    if (Party_AddMon(SaveArray_Party_Get(fieldSys->saveData), pokemon)) {
+        UpdatePokedexWithReceivedSpecies(fieldSys->saveData, pokemon);
+    }
+    if (tmpPokemon != NULL) {
+        FreeToHeap(tmpPokemon);
+    }
 }
