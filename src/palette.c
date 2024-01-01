bf688f84a3d7cfa740b463342830de906210ff22
PikalaxALT <pikalaxalt@users.noreply.github.com>
2024-01-01 06:45:22 -0500

    palette.c through sub_020035B4

--- src/palette.c
+++ src/palette.c
@@ -2,6 +2,14 @@
 #include "palette.h"
 #include "gf_gfx_loader.h"
 #include "unk_02026E84.h"
+#include "sys_task_api.h"
+
+u8 IsPaletteSelected(u16 toSelect, u16 bufferID);
+void sub_02003574(PaletteData *data, u16 bufferID);
+void sub_020035B4(int bufferID, PaletteBuffer *buffer, u16 *opaqueBit);
+void sub_020035F0(SelectedPaletteData *selectedBit, u16 opaqueBit, s8 wait, u8 cur, u8 end, u16 nextRGB);
+void sub_020036B0(SysTask *task, void *data);
+void sub_020037FC(PaletteData *data, u16 bufferID, u16 flags);
 
 PaletteData *PaletteData_Init(HeapID heapId) {
     PaletteData *ret = AllocFromHeap(heapId, sizeof(PaletteData));
@@ -13,27 +21,27 @@ void PaletteData_Free(PaletteData *plttData) {
     FreeToHeap(plttData);
 }
 
-void PaletteData_SetBuffers(PaletteData *data, u32 bufferID, u16 *opaque, u16 *transparent, u32 size) {
+void PaletteData_SetBuffers(PaletteData *data, int bufferID, u16 *opaque, u16 *transparent, u32 size) {
     data->buffers[bufferID].opaque = opaque;
     data->buffers[bufferID].transparent = transparent;
     data->buffers[bufferID].size = size;
 }
 
-void PaletteData_AllocBuffers(PaletteData *data, u32 bufferID, u32 size, HeapID heapID) {
+void PaletteData_AllocBuffers(PaletteData *data, int bufferID, u32 size, HeapID heapID) {
     PaletteData_SetBuffers(data, bufferID, AllocFromHeap(heapID, size), AllocFromHeap(heapID, size), size);
 }
 
-void PaletteData_FreeBuffers(PaletteData *data, u32 bufferID) {
+void PaletteData_FreeBuffers(PaletteData *data, int bufferID) {
     FreeToHeap(data->buffers[bufferID].opaque);
     FreeToHeap(data->buffers[bufferID].transparent);
 }
 
-void PaletteData_LoadPalette(PaletteData *data, const u16 *src, u32 bufferID, u16 pos, u16 size) {
+void PaletteData_LoadPalette(PaletteData *data, const u16 *src, int bufferID, u16 pos, u16 size) {
     MI_CpuCopy16(src, data->buffers[bufferID].opaque + pos, size);
     MI_CpuCopy16(src, data->buffers[bufferID].transparent + pos, size);
 }
 
-void PaletteData_LoadFromNarc(PaletteData *data, NarcId narcID, s32 memberNo, HeapID heapID, u32 bufferID, u32 size, u16 pos, u16 readPos) {
+void PaletteData_LoadFromNarc(PaletteData *data, NarcId narcID, s32 memberNo, HeapID heapID, int bufferID, u32 size, u16 pos, u16 readPos) {
     void *rawPtr;
     NNSG2dPaletteData *plttData;
 
@@ -47,11 +55,11 @@ void PaletteData_LoadFromNarc(PaletteData *data, NarcId narcID, s32 memberNo, He
     FreeToHeap(rawPtr);
 }
 
-void PaletteData_LoadNarc(PaletteData *data, NarcId narcID, s32 memberNo, HeapID heapID, u32 bufferID, u32 size, u16 pos) {
+void PaletteData_LoadNarc(PaletteData *data, NarcId narcID, s32 memberNo, HeapID heapID, int bufferID, u32 size, u16 pos) {
     PaletteData_LoadFromNarc(data, narcID, memberNo, heapID, bufferID, size, pos, 0);
 }
 
-void PaletteData_LoadFromOpenNarc(PaletteData *data, NARC *narc, s32 memberNo, HeapID heapID, u32 bufferID, u32 size, u16 pos, u16 readPos) {
+void PaletteData_LoadFromOpenNarc(PaletteData *data, NARC *narc, s32 memberNo, HeapID heapID, int bufferID, u32 size, u16 pos, u16 readPos) {
     void *rawPtr;
     NNSG2dPaletteData *plttData;
 
@@ -65,11 +73,11 @@ void PaletteData_LoadFromOpenNarc(PaletteData *data, NARC *narc, s32 memberNo, H
     FreeToHeap(rawPtr);
 }
 
-void PaletteData_LoadOpenNarc(PaletteData *data, NARC *narc, s32 memberNo, HeapID heapID, u32 bufferID, u32 size, u16 pos) {
+void PaletteData_LoadOpenNarc(PaletteData *data, NARC *narc, s32 memberNo, HeapID heapID, int bufferID, u32 size, u16 pos) {
     PaletteData_LoadFromOpenNarc(data, narc, memberNo, heapID, bufferID, size, pos, 0);
 }
 
-void sub_020032A4(PaletteData *data, u32 bufferID, u16 pos, u32 size) {
+void sub_020032A4(PaletteData *data, int bufferID, u16 pos, u32 size) {
     GF_ASSERT(pos * sizeof(pos) + size <= data->buffers[bufferID].size);
 
     const u16 *src;
@@ -99,11 +107,101 @@ void PaletteData_CopyPalette(PaletteData *data, u32 srcBufferID, u16 srcPos, u32
     MI_CpuCopy16(data->buffers[srcBufferID].opaque + srcPos, data->buffers[destBufferID].transparent + destPos, size);
 }
 
-u16 *PaletteData_GetUnfadedBuf(PaletteData *data, u32 bufferID) {
+u16 *PaletteData_GetUnfadedBuf(PaletteData *data, int bufferID) {
     return data->buffers[bufferID].opaque;
 }
 
-u16 *PaletteData_GetFadedBuf(PaletteData *data, u32 bufferID) {
+u16 *PaletteData_GetFadedBuf(PaletteData *data, int bufferID) {
     return data->buffers[bufferID].transparent;
 }
 
+u8 sub_02003370(PaletteData *data, u16 toSelect, u16 opaqueBit, s8 wait, u8 cur, u8 end, u16 nextRGB) {
+    u16 opaqueBitBak = opaqueBit;
+    u8 r6 = FALSE;
+    u8 i;
+
+    for (i = 0; i < 14; ++i) {
+        if (IsPaletteSelected(toSelect, i) == TRUE
+         && IsPaletteSelected(data->selectedBuffer, i) == FALSE) {
+            sub_020035B4(i, &data->buffers[i], &opaqueBit);
+            sub_020035F0(&data->buffers[i].selected, opaqueBit, wait, cur, end, nextRGB);
+            sub_02003574(data, i);
+            if (i >= 4) {
+                sub_020037FC(data, i, 0x100);
+            } else {
+                sub_020037FC(data, i, 0x10);
+            }
+            opaqueBit = opaqueBitBak;
+            r6 = TRUE;
+        }
+    }
+
+    if (r6 == TRUE) {
+        data->selectedBuffer |= toSelect;
+        if (!data->callbackFlag) {
+            data->callbackFlag = TRUE;
+            data->selectedFlag = 1;
+            data->forceExit = FALSE;
+            CreateSysTask(sub_020036B0, data, -2);
+        }
+    }
+
+    return r6;
+}
+
+u8 sub_02003474(PaletteData *data, u16 toSelect, u16 opaqueBit, s8 wait, u8 cur, u8 end, u16 nextRGB) {
+    u16 opaqueBitBak = opaqueBit;
+    u8 r6 = FALSE;
+    u8 i;
+
+    for (i = 0; i < 14; ++i) {
+        if (IsPaletteSelected(toSelect, i) == TRUE) {
+            sub_020035B4(i, &data->buffers[i], &opaqueBit);
+            sub_020035F0(&data->buffers[i].selected, opaqueBit, wait, cur, end, nextRGB);
+            sub_02003574(data, i);
+            if (i >= 4) {
+                sub_020037FC(data, i, 0x100);
+            } else {
+                sub_020037FC(data, i, 0x10);
+            }
+            opaqueBit = opaqueBitBak;
+            r6 = TRUE;
+        }
+    }
+
+    if (r6 == TRUE) {
+        data->selectedBuffer = toSelect;
+        if (!data->callbackFlag) {
+            data->callbackFlag = TRUE;
+            data->selectedFlag = 1;
+            data->forceExit = FALSE;
+            CreateSysTask(sub_020036B0, data, -2);
+        }
+    }
+
+    return r6;
+}
+
+u8 IsPaletteSelected(u16 selectedFlag, u16 bufferID) {
+    return (selectedFlag & (1 << bufferID)) != 0;
+}
+
+void sub_02003574(PaletteData *data, u16 bufferID) {
+    if (IsPaletteSelected(data->transparentBit, bufferID) != TRUE) {
+        data->transparentBit |= 1 << bufferID;
+    }
+}
+
+void sub_020035B4(int bufferID, PaletteBuffer *buffer, u16 *opaqueBit) {
+    u8 limit;
+    if (bufferID < 4) {
+        limit = buffer->size >> 5;
+    } else {
+        limit = buffer->size >> 9;
+    }
+    u16 bits = 0;
+    for (u8 i = 0; i < limit; ++i) {
+        bits += (1 << i);
+    }
+    *opaqueBit &= bits;
+}
