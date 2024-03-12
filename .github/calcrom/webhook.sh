64d78e239a56be744b927037b790a2d3bed6616c
Abaresk <abaresk01@gmail.com>
2024-03-12 05:58:50 +0000

    Test calcrom

--- .github/calcrom/webhook.sh
+++ .github/calcrom/webhook.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
 # Only run this script if it's the master branch build.
-if [[ "$GITHUB_REF" != "refs/heads/master" || "$GITHUB_EVENT_NAME" != "push" ]]; then
-  exit 0
-fi
+# if [[ "$GITHUB_REF" != "refs/heads/master" || "$GITHUB_EVENT_NAME" != "push" ]]; then
+#   exit 0
+# fi
 
 build_name=$1
 url=$2
@@ -17,4 +17,5 @@ fi
 
 make -C ${GITHUB_WORKSPACE}/.github/calcrom
 output=$(${GITHUB_WORKSPACE}/.github/calcrom/calcrom -d ${GITHUB_WORKSPACE} -9 "" -7 sub heartgold.us soulsilver.us | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')
-curl -d "{\"username\": \"$CALCROM_DISCORD_WEBHOOK_USERNAME\", \"avatar_url\": \"$CALCROM_DISCORD_WEBHOOK_AVATAR_URL\", \"content\":\"\`\`\`$build_name progress:\\n$output\`\`\`\"}" -H "Content-Type: application/json" -X POST $url
+echo $output
+# curl -d "{\"username\": \"$CALCROM_DISCORD_WEBHOOK_USERNAME\", \"avatar_url\": \"$CALCROM_DISCORD_WEBHOOK_AVATAR_URL\", \"content\":\"\`\`\`$build_name progress:\\n$output\`\`\`\"}" -H "Content-Type: application/json" -X POST $url
