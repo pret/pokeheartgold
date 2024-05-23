6eb467220abdb8dd81c8753976dc983dba61b1a8
PikalaxALT <pikalaxalt@users.noreply.github.com>
2024-05-23 07:22:07 -0400

    Dump a/2/6/1

--- filesystem.mk
+++ filesystem.mk
@@ -450,6 +450,7 @@ $(eval $(call arc_strip_name,files/demo/opening/gs_opening.narc,files/a/2/6/2))
 $(eval $(call arc_strip_name,files/application/voltorb_flip.narc,files/a/2/6/4))
 $(eval $(call arc_strip_name,files/poketool/icongra/poke_icon/poke_icon.narc,files/a/0/2/0))
 $(eval $(call arc_strip_name,files/data/photo_data.narc,files/a/2/5/4))
+$(eval $(call arc_strip_name,files/graphic/camera_viewfinder.narc,files/a/2/6/1))
 
 $(DIFF_ARCS):
 	cp $< $@
@@ -511,6 +512,7 @@ include files/arc/headbutt.mk
 include files/poketool/personal/personal.mk
 include files/fielddata/graphic/preview_graphic/preview_graphic.mk
 include files/data/photo_data.mk
+include files/graphic/camera_viewfinder.mk
 
 $(filter-out $(DIFF_ARCS) $(FS_RULE_OVERRIDES),$(NITROFS_FILES)): ;
 
