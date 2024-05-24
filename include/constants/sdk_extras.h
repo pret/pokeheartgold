2431e23fa6810f91a8771ae14b6ac55c05f7c1c0
PikalaxALT <pikalaxalt@users.noreply.github.com>
2024-05-24 19:34:17 -0400

    fix building

--- include/constants/sdk_extras.h
+++ include/constants/sdk_extras.h
@@ -1,13 +1,12 @@
 #ifndef POKEHEARTGOLD_CONSTANTS_SDK_EXTRAS_H
 #define POKEHEARTGOLD_CONSTANTS_SDK_EXTRAS_H
 
-#include <nnsys.h>
-
 // Header for constants that play the role of SDK defines but are not actually provided by any SDK
 // Any define that should be typed, must be gated behind defined(PM_ASM)
 
 // alias
 #ifndef PM_ASM
+#include <nnsys.h>
 // NNS_G2D_VRAM_TYPE_3DMAIN
 #define NNS_G2D_VRAM_TYPE_NEITHER   ((NNS_G2D_VRAM_TYPE)0)
 // NNS_G2D_VRAM_TYPE_MAX
