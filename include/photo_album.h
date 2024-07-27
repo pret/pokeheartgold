4e79281e914c9ea1f8d5e86a8b2bd1af3429c521
PikalaxALT <pikalaxalt@users.noreply.github.com>
2024-07-26 22:57:55 -0400

    fix photo_album.h indentation

--- include/photo_album.h
+++ include/photo_album.h
@@ -7,58 +7,58 @@
 #define PHOTO_ALBUM_MAX 36
 
 typedef struct PhotoMon {
-  u16 species;
-  u8 form;
-  u8 shiny:1;
-  u8 gender:1;
+    u16 species;
+    u8 form;
+    u8 shiny:1;
+    u8 gender:1;
 } PhotoMon;
 
 typedef struct PhotoCameraParam {
-  fx32 distance;
-  CameraAngle angle;
-  u16 perspectiveType;
-  u16 perspective;
-  int unk_10[2];
-  VecFx32 lookAt;
+    fx32 distance;
+    CameraAngle angle;
+    u16 perspectiveType;
+    u16 perspective;
+    int unk_10[2];
+    VecFx32 lookAt;
 } PhotoCameraParam;
 
 typedef struct Photo {
-  u8 filler_0[4];
-  u8 gender : 1;
-  u8 iconId : 7;
-  u8 filler_5[2];
-  u8 numMons;
-  u16 playerName[8];
-  u16 leadMonNick[12];
-  u8 avatarStateBak;
-  u16 mapId;
-  u16 x;
-  u16 y;
-  u32 date;
-  u16 hour;
-  u16 min;
-  u16 unk_40[2];
-  u16 subjectSpriteId;
-  u16 unk_46;
-  PhotoCameraParam unk_48;
-  PhotoMon party[PARTY_SIZE];
+    u8 filler_0[4];
+    u8 gender : 1;
+    u8 iconId : 7;
+    u8 filler_5[2];
+    u8 numMons;
+    u16 playerName[8];
+    u16 leadMonNick[12];
+    u8 avatarStateBak;
+    u16 mapId;
+    u16 x;
+    u16 y;
+    u32 date;
+    u16 hour;
+    u16 min;
+    u16 unk_40[2];
+    u16 subjectSpriteId;
+    u16 unk_46;
+    PhotoCameraParam unk_48;
+    PhotoMon party[PARTY_SIZE];
 } Photo; // size: 0x84
 
 // file: a/2/5/4
 typedef struct PhotoData {
-  u16 mapId;
-  u16 iconId;
-  u16 x;
-  u16 y;
-  u8 unk8;
-  u8 unk9;
-  u16 subjectObjId;
-  u16 param[2];
+    u16 mapId;
+    u16 iconId;
+    u16 x;
+    u16 y;
+    u8 unk8;
+    u8 unk9;
+    u16 subjectObjId;
+    u16 param[2];
 } PhotoData;
 
 typedef struct PhotoAlbum {
-  int unk_00;
-  Photo photos[PHOTO_ALBUM_MAX];
+    int unk_00;
+    Photo photos[PHOTO_ALBUM_MAX];
 } PhotoAlbum;
 
 u32 Save_PhotoAlbum_sizeof(void);
