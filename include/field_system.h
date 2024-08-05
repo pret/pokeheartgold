cecc98b6d469aa9f60b12bea527d4585201be9dc
PikalaxALT <pikalaxalt@users.noreply.github.com>
2024-08-04 23:54:50 -0400

    fix build errors resulting from bad merge

--- include/field_system.h
+++ include/field_system.h
@@ -35,7 +35,7 @@ typedef struct FollowMon {
     u32 unk1C;
 } FollowMon;
 
-typedef struct GearPhoneRingManager {
+struct GearPhoneRingManager {
     u8 unk_var0_0:1;
     u8 unk_var0_1:1;
     u8 unk_var0_2:1;
@@ -61,7 +61,7 @@ typedef struct GearPhoneRingManager {
         SysTask *task;
         u8 counter;
     } gearRing;
-} GearPhoneRingManager; //size: 0x48
+}; //size: 0x48
 
 typedef struct FieldSystemUnk108 {
     u32 personality;
