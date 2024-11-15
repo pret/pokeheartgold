a7d431920240ca536a1a13c65d1f1aac0e52f195
tgsm <doodrabbit@hotmail.com>
2024-11-15 10:59:11 -0600

    application/delete_save: Remove unused files

--- include/application/delete_savedata.h
+++ include/application/delete_savedata.h
@@ -3,6 +3,8 @@
 
 #include "overlay_manager.h"
 
+extern const OVY_MGR_TEMPLATE gApplication_DeleteSave;
+
 BOOL DeleteSavedataApp_Init(OVY_MANAGER *manager, int *state);
 BOOL DeleteSavedataApp_Main(OVY_MANAGER *manager, int *state);
 BOOL DeleteSavedataApp_Exit(OVY_MANAGER *manager, int *state);
