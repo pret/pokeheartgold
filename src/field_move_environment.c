b6bd05e29b6f4fc33a054ab7fff4e6b43c3345b7
red031000 <rubenru09@aol.com>
2025-02-15 04:09:36 +0000

    address review comments

--- src/field_move_environment.c
+++ src/field_move_environment.c
@@ -7,8 +7,8 @@
 #include "save.h"
 
 FieldMoveEnvironment *FieldMove_CreateEnvironment(HeapID heapId, u8 slotNo, SaveData *saveData) {
-    FieldMoveEnvironment *environmemt = AllocFromHeapAtEnd(heapId, sizeof(FieldMoveEnvironment));
-    environmemt->mon = Party_GetMonByIndex(SaveArray_Party_Get(saveData), slotNo);
-    environmemt->moveData = NULL;
-    return environmemt;
+    FieldMoveEnvironment *environment = AllocFromHeapAtEnd(heapId, sizeof(FieldMoveEnvironment));
+    environment->mon = Party_GetMonByIndex(SaveArray_Party_Get(saveData), slotNo);
+    environment->moveData = NULL;
+    return environment;
 }
