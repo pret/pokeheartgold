76a297951a14ccce2a44f9e3713d78faa375e500
red031000 <rubenru09@aol.com>
2025-02-24 23:02:02 +0000

    split MSL_Common_wchar_io.s

--- lib/asm/msl.s
+++ lib/asm/msl.s
@@ -24,45 +24,6 @@ __global_destructor_chain: ; 0x021E58E8
 
 	; MSL_C_NITRO_Ai_LE.a
 
-	arm_func_start fwide
-fwide: ; 0x020EB840
-	cmp r0, #0
-	beq _020EB858
-	ldr r3, [r0, #4]
-	mov r2, r3, lsl #0x16
-	movs r2, r2, lsr #0x1d
-	bne _020EB860
-_020EB858:
-	mov r0, #0
-	bx lr
-_020EB860:
-	mov r2, r3, lsl #0x14
-	movs r2, r2, lsr #0x1e
-	beq _020EB880
-	cmp r2, #1
-	beq _020EB8A8
-	cmp r2, #2
-	moveq r1, #1
-	b _020EB8AC
-_020EB880:
-	cmp r1, #0
-	ble _020EB898
-	bic r2, r3, #0xc00
-	orr r2, r2, #0x800
-	str r2, [r0, #4]
-	b _020EB8AC
-_020EB898:
-	biclt r2, r3, #0xc00
-	orrlt r2, r2, #0x400
-	strlt r2, [r0, #4]
-	b _020EB8AC
-_020EB8A8:
-	mvn r1, #0
-_020EB8AC:
-	mov r0, r1
-	bx lr
-	arm_func_end fwide
-
 	arm_func_start wmemcpy
 wmemcpy: ; 0x020EB8B4
 	ldr ip, _020EB8C0 ; =memcpy
