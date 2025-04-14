f0e25299a6392753006ba334a348a24723d7cdae
PikalaxALT <PikalaxALT@users.noreply.github.com>
2025-04-13 21:05:59 -0400

    through ov100_021E6D34

--- src/application/pokegear/phone/overlay_100_021E6914.c
+++ src/application/pokegear/phone/overlay_100_021E6914.c
@@ -14,13 +14,14 @@ typedef struct UnkStruct_ov100_021E76BC {
 
 void ov100_021E6978(PokegearPhoneApp *phoneApp, int a1);
 void ov100_021E69C8(PokegearPhoneApp *phoneApp);
-void ov100_021E6CF4(PokegearPhoneApp_UnkSub094 *a0);
 void ov100_021E6A58(PokegearPhoneApp_UnkSub094 *a0, int a1);
+void ov100_021E6C4C(PokegearPhoneApp_UnkSub094 *a0, u16 a1);
+void ov100_021E6CF4(PokegearPhoneApp_UnkSub094 *a0);
+void ov100_021E6D34(PokegearPhoneApp_UnkSub094 *a0, u16 a1);
 
 extern const u8 ov100_021E764C[];
 extern const u8 ov100_021E770C[];
 extern const UnkStruct_ov100_021E76BC ov100_021E76BC[];
-void ov100_021E6C4C(PokegearPhoneApp_UnkSub094 *a0, u16 a1);
 
 // -ipo file shenanigans
 #define ov100_021E76BC_ipo ((const UnkStruct_ov100_021E76BC *)(ov100_021E764C + 0x70))
@@ -141,4 +142,61 @@ UnkImageStruct *ov100_021E6AC0(PokegearPhoneApp_UnkSub094 *a0, u8 x, u8 y, u8 z,
 
 void ov100_021E6C44(UnkImageStruct *a0) {
     UnkImageStruct_Delete(a0);
+}
+
+void ov100_021E6C4C(PokegearPhoneApp_UnkSub094 *a0, u16 a1) {
+    extern const u8 ov100_021E764C[4];
+    u8 spC[4];
+    ARRAY_ASSIGN(spC, ov100_021E764C);
+    a0->unk_010 = G2dRenderer_Init(a0->unk_00C, &a0->unk_014, a0->heapId);
+    spC[1] = a0->unk_00A;
+    for (u32 i = 0; i < 4; ++i) {
+        a0->unk_140[i] = Create2DGfxResObjMan(spC[i], (GfGfxResType)i, a0->heapId);
+        a0->unk_150[i] = Create2DGfxResObjList(spC[i], a0->heapId);
+        for (u32 j = 0; j < a0->unk_150[i]->max; ++j) {
+            a0->unk_150[i]->obj[j] = NULL;
+        }
+    }
+    ov100_021E6D34(a0, a1);
+}
+
+void ov100_021E6CF4(PokegearPhoneApp_UnkSub094 *a0) {
+    SpriteList_Delete(a0->unk_010);
+    sub_0200AED4(a0->unk_150[GF_GFX_RES_TYPE_CHAR]);
+    sub_0200B0CC(a0->unk_150[GF_GFX_RES_TYPE_PLTT]);
+    for (u32 i = 0; i < 4; ++i) {
+        Delete2DGfxResObjList(a0->unk_150[i]);
+        Destroy2DGfxResObjMan(a0->unk_140[i]);
+    }
+}
+
+void ov100_021E6D34(PokegearPhoneApp_UnkSub094 *a0, u16 a1) {
+    GF_2DGfxResObjList *objList;
+
+    objList = a0->unk_150[GF_GFX_RES_TYPE_CHAR];
+    objList->obj[0] = AddCharResObjFromNarc(a0->unk_140[GF_GFX_RES_TYPE_CHAR], NARC_a_1_4_3, a1 + 6, FALSE, 0xE000, (NNS_G2D_VRAM_TYPE)a0->unk_00E, a0->heapId);
+    GF_ASSERT(objList->obj[0] != NULL);
+    switch (a0->unk_004) {
+    case 1:
+        sub_0200ADA4(objList->obj[0]);
+        break;
+    case 2:
+        sub_0200AE18(objList->obj[0]);
+        break;
+    case 0:
+    default:
+        sub_0200ACF0(objList->obj[0]);
+        break;
+    }
+
+    for (int i = 0; i < 2; ++i) {
+        objList = a0->unk_150[GF_GFX_RES_TYPE_CELL + i];
+        objList->obj[0] = AddCellOrAnimResObjFromNarc(a0->unk_140[GF_GFX_RES_TYPE_CELL + i], NARC_a_1_4_3, 12 + i, FALSE, 0xE000, (GfGfxResType)(GF_GFX_RES_TYPE_CELL + i), a0->heapId);
+        GF_ASSERT(objList->obj[0] != NULL);
+    }
+
+    objList = a0->unk_150[GF_GFX_RES_TYPE_PLTT];
+    objList->obj[0] = AddPlttResObjFromNarc(a0->unk_140[GF_GFX_RES_TYPE_PLTT], NARC_a_1_4_3, a1, FALSE, 0xE000, (NNS_G2D_VRAM_TYPE)a0->unk_00E, 4, a0->heapId);
+    GF_ASSERT(objList->obj[0] != NULL);
+    sub_0200B00C(objList->obj[0]);
 }
