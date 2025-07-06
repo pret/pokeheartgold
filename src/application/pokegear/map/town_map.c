cbb27dd585ef7574a85b459873615b07410ba36c
PikalaxALT <PikalaxALT@users.noreply.github.com>
2025-07-06 15:31:16 -0500

    ov101_021ED980

--- src/application/pokegear/map/town_map.c
+++ src/application/pokegear/map/town_map.c
@@ -1,6 +1,8 @@
 #include "application/pokegear/map/pokegear_map_internal.h"
 
+#include "phonebook_dat.h"
 #include "pokegear_apps.h"
+#include "sys_flags.h"
 
 FS_EXTERN_OVERLAY(OVY_26);
 FS_EXTERN_OVERLAY(OVY_100);
@@ -13,6 +15,8 @@ int ov101_021EDBD4(PokegearMapAppData *mapApp);
 int ov101_021EDBFC(PokegearMapAppData *mapApp);
 int ov101_021EDC04(PokegearMapAppData *mapApp);
 int ov101_021EDC70(PokegearMapAppData *mapApp);
+
+extern const u16 ov101_021F7F3C[];
 
 BOOL TownMap_Init(OverlayManager *man, int *state) {
     PokegearArgs *args = OverlayManager_GetArgs(man);
@@ -81,4 +85,38 @@ BOOL TownMap_Exit(OverlayManager *man, int *state) {
     UnloadOverlayByID(FS_OVERLAY_ID(OVY_26));
     UnloadOverlayByID(FS_OVERLAY_ID(OVY_100));
     return TRUE;
+}
+
+void ov101_021ED980(PokegearMapAppData *mapApp) {
+    mapApp->pokegear->childAppdata = mapApp;
+    mapApp->pokegear->menuInputState = (MenuInputState)mapApp->pokegear->args->unk01;
+    mapApp->unk_00D = mapApp->pokegear->args->incomingPhoneCall + 1;
+    mapApp->pokegear->reselectAppCB = ov101_021EB338;
+    mapApp->pokegear->unknownCB = ov101_021EB2FC;
+    ov101_021ED4E0(mapApp);
+    mapApp->unk_138_4 = 0;
+    mapApp->unk_138_1 = 0;
+    mapApp->unk_138_5 = Pokegear_GetMapUnlockLevel(mapApp->pokegear->savePokegear);
+    mapApp->unk_038 = MapMatrix_MapData_New(mapApp->heapId);
+    mapApp->unk_124 = SaveData_GetPhoneCallPersistentState(mapApp->pokegear->saveData);
+    mapApp->unk_128 = AllocAndReadPhoneBook(mapApp->heapId);
+    mapApp->unk_12C = SavePokegear_AllocAndCopyPhonebook(mapApp->pokegear->savePokegear, mapApp->heapId);
+    mapApp->unk_130 = SavePokegear_FindEmptyPhonebookSlot(mapApp->pokegear->savePokegear);
+    mapApp->unk_108 = mapApp->pokegear->args->matrixXCoord;
+    mapApp->unk_10A = mapApp->pokegear->args->matrixYCoord + 2;
+    mapApp->unk_10C = mapApp->pokegear->args->mapID;
+    mapApp->unk_10E = mapApp->pokegear->args->playerGender;
+    mapApp->unk_110 = mapApp->pokegear->args->matrixXCoord;
+    mapApp->unk_112 = mapApp->pokegear->args->matrixYCoord + 2;
+    mapApp->unk_100 = 1;
+    mapApp->unk_104 = 1;
+    mapApp->unk_102 = ov101_021F7F3C[mapApp->unk_138_5];
+    mapApp->unk_106 = 0x11;
+    mapApp->unk_132 = 0x20;
+    mapApp->unk_131 = 8;
+    mapApp->unk_133 = 0;
+    mapApp->unk_134 = -24;
+    mapApp->unk_13D_1 = TRUE;
+    mapApp->unk_13D_0 = Save_VarsFlags_FlypointFlagAction(mapApp->pokegear->saveVarsFlags, FLAG_ACTION_CHECK, FLYPOINT_GOLDENROD);
+    mapApp->unk_00E = ov100_021E5C80(mapApp->pokegear);
 }
