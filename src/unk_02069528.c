8cf81a73006e20db139e3e2dfbbf7bad99b86498
Ander Aginaga / Laquin <anderaginaga2@gmail.com>
2025-08-17 19:28:15 +0200

    Clean up code

--- src/unk_02069528.c
+++ src/unk_02069528.c
@@ -20,20 +20,18 @@ void sub_02069528(SaveData* saveData, s32 arg2, UnkStruct_02069528* arg3) {
     MI_CpuFill8(arg3, 0, sizeof(UnkStruct_02069528));
     MI_CpuCopy8(PlayerProfile_GetNamePtr(playerProfile), &arg3->playerName, (PLAYER_NAME_LENGTH + 1) * sizeof(u16));
     arg3->trainerId = PlayerProfile_GetTrainerID(playerProfile);
-    arg3->gameVersion = (u8) gGameVersion;
-    arg3->gameLanguage = (u8) gGameLanguage;
+    arg3->gameVersion = gGameVersion;
+    arg3->gameLanguage = gGameLanguage;
     arg3->playerCountry = WifiHistory_GetPlayerCountry(saveWiFiHistory);
     arg3->playerRegion = WiFiHistory_GetPlayerRegion(saveWiFiHistory);
-    arg3->unkC8 = (u8) ((arg3->unkC8 & ~2) | ((u32) (((PlayerProfile_GetTrainerGender(playerProfile) << 0x18) >> 0x18) << 0x1F) >> 0x1E));
+    arg3->unkC8 = ((arg3->unkC8 & ~2) | (((u32)(u8)(PlayerProfile_GetTrainerGender(playerProfile)) & 1) << 1));
     arg3->trainerClass = GetUnionRoomAvatarAttrBySprite((u32) (arg3->unkC8 << 0x1E) >> 0x1F, PlayerProfile_GetAvatar(playerProfile), 1);
     
     index = 0;
     position = 0;
-    do {
+    for (index = 0, position = 0; index < 3; index++, position += 8) {
         MI_CpuCopy8(sub_0202D660(saveData, index), &arg3->unkCA[position], 8);
-        index += 1;
-        position += 8;
-    } while (index < 3);
+    }
     
     MI_CpuCopy8(sub_0202D660(saveData, 3), &arg3->unkC0, sizeof(arg3->unkC0));
     
