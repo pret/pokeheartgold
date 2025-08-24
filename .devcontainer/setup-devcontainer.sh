480e474df87cd29bff29b245c20e517114f115a9
PikalaxALT <PikalaxALT@users.noreply.github.com>
2025-08-24 13:41:36 -0400

    Install wine-mono; conditional dependency download; rebuild tools in container

--- .devcontainer/setup-devcontainer.sh
+++ .devcontainer/setup-devcontainer.sh
@@ -6,13 +6,21 @@ GITHUB_WORKSPACE="${GITHUB_WORKSPACE:-"${PWD}"}"
 DLDIR=$(mktemp -d)
 trap 'rm -rf $DLDIR' EXIT
 pushd "$DLDIR"
-wget https://github.com/pret/pokeheartgold/raw/workflows/assets/mwccarm.zip
-wget https://github.com/pret/pokeheartgold/raw/workflows/assets/NitroSDK-3_2-060901.7z
-unzip mwccarm.zip
-mv mwccarm $GITHUB_WORKSPACE/tools
-7z x NitroSDK-3_2-060901.7z
-mv NitroSDK-3_2-060901/tools/bin $GITHUB_WORKSPACE/tools
-mv NitroSDK-3_2-060901/include/nitro/specfiles/ARM7-TS.lcf.template $GITHUB_WORKSPACE/sub/
-mv NitroSDK-3_2-060901/include/nitro/specfiles/ARM9-TS.lcf.template $GITHUB_WORKSPACE/
-mv NitroSDK-3_2-060901/include/nitro/specfiles/mwldarm.response.template $GITHUB_WORKSPACE/
+
+if ! test -d $GITHUB_WORKSPACE/tools/mwccarm; then
+    wget https://github.com/pret/pokeheartgold/raw/workflows/assets/mwccarm.zip
+    unzip mwccarm.zip
+    mv mwccarm $GITHUB_WORKSPACE/tools
+fi
+
+if ! test -d $GITHUB_WORKSPACE/tools/bin; then
+    wget https://github.com/pret/pokeheartgold/raw/workflows/assets/NitroSDK-3_2-060901.7z
+    7z x NitroSDK-3_2-060901.7z
+    mv NitroSDK-3_2-060901/tools/bin $GITHUB_WORKSPACE/tools
+    mv NitroSDK-3_2-060901/include/nitro/specfiles/ARM7-TS.lcf.template $GITHUB_WORKSPACE/sub/
+    mv NitroSDK-3_2-060901/include/nitro/specfiles/ARM9-TS.lcf.template $GITHUB_WORKSPACE/
+    mv NitroSDK-3_2-060901/include/nitro/specfiles/mwldarm.response.template $GITHUB_WORKSPACE/
+fi
+
 popd
+make clean-tools && make -j$(nproc) tools
