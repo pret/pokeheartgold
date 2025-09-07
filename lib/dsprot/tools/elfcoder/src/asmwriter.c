804fe98ef0b37881732bff4c330d64cba6aa2439
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-07 15:53:30 -0400

    Update dsprot generated assembly

--- lib/dsprot/tools/elfcoder/src/asmwriter.c
+++ lib/dsprot/tools/elfcoder/src/asmwriter.c
@@ -95,16 +95,17 @@ static void writeAssembly(ASMWriter_Ctx *asmw, FILE *output) {
         }
 
         fprintf(output,
-            "\tfunc_table_end\n"
-            "\tarm_func_end NitroStaticInit\n"
-            "\n");
+            "\tfunc_table_end\n");
 
         if (asmw->garbage != NULL) {
             fprintf(output,
-                "\tgarbage_ref %s\n"
-                "\n",
+                "\tgarbage_ref %s\n",
                 asmw->garbage);
         }
+
+        fprintf(output,
+            "\tarm_func_end NitroStaticInit\n"
+            "\n");
 
         fprintf(output,
             "\t.section .sinit, 4\n"
