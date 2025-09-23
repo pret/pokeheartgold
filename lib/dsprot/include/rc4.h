1a9f4eaba1714131daed0e835e42c1ec05ce3a9c
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2025-09-22 22:47:46 -0400

    Clarify encoded functions must be global

--- lib/dsprot/include/rc4.h
+++ lib/dsprot/include/rc4.h
@@ -3,17 +3,6 @@
 
 #include "nitro_types.h"
 
-typedef struct {
-    int i;
-    int j;
-    u8 S[256];
-} RC4_Ctx;
-
-void RC4_Init(RC4_Ctx *ctx, const void *key, u32 key_len);
-u8 RC4_Byte(RC4_Ctx *ctx);
-u32 RC4_InitSBox(u8 *sbox);
-u32 RC4_EncryptInstructions(RC4_Ctx *ctx, void *src, void *dst, u32 size);
-u32 RC4_DecryptInstructions(RC4_Ctx *ctx, void *src, void *dst, u32 size);
 u32 RC4_InitAndEncryptInstructions(void *key, void *dst, void *src, u32 size);
 u32 RC4_InitAndDecryptInstructions(void *key, void *dst, void *src, u32 size);
 
--- lib/dsprot/src/rc4.c
+++ lib/dsprot/src/rc4.c
@@ -5,6 +5,19 @@
 
 #define RC4_KEY_SIZE (16)
 
+typedef struct {
+    int i;
+    int j;
+    u8 S[256];
+} RC4_Ctx;
+
+// Functions to be encoded (cannot be static)
+void RC4_Init(RC4_Ctx *ctx, const void *key, u32 key_len);
+u8 RC4_Byte(RC4_Ctx *ctx);
+u32 RC4_InitSBox(u8 *sbox);
+u32 RC4_EncryptInstructions(RC4_Ctx *ctx, void *src, void *dst, u32 size);
+u32 RC4_DecryptInstructions(RC4_Ctx *ctx, void *src, void *dst, u32 size);
+
 void RC4_Init(RC4_Ctx *ctx, const void *key, u32 key_len) {
     s32 Ki = 0;
     s32 Si = 0;
