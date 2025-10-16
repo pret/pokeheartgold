111f12f44c505a53308610b4d85b542b6364b120
PikalaxALT <PikalaxALT@users.noreply.github.com>
2025-10-16 07:53:35 -0400

    Move voltorb flip data 1, 2 to voltorb_flip.c

--- src/voltorb_flip/voltorb_flip.c
+++ src/voltorb_flip/voltorb_flip.c
@@ -141,7 +141,6 @@ static void FreeOverlayData(OverlayManager *);
 
 extern const GraphicsBanks sVoltorbFlipGraphicsBanks;
 extern const MsgNoList sMenuMsgNos[];
-extern const Ov122_021E9278 ov122_021E9278;
 extern const u16 ov122_021E92A0[8];
 extern const u8 ov122_021E92B0[4][4];
 extern const struct GraphicsModes sVoltorbFlipBgModeSet;
@@ -154,6 +153,409 @@ extern const ManagedSpriteTemplate ov122_021E93A8;
 extern const WindowTemplate sVoltorbFlipWindowTemplates[];
 extern const BgTemplates sVoltorbFlipBgTemplates;
 extern VoltorbFlipWorkflows sVoltorbFlipWorkflows;
+
+const u8 sMainMenuMsgNos[] = { msg_0039_00017, msg_0039_00018, msg_0039_00019 };
+const u8 sGameInfoMsgNos[] = { msg_0039_00021, msg_0039_00022, msg_0039_00023, msg_0039_00024 };
+
+const u8 ov122_021E9270[] = {
+    GF_BG_LYR_MAIN_0,
+    GF_BG_LYR_MAIN_1,
+    GF_BG_LYR_MAIN_2,
+    GF_BG_LYR_MAIN_3,
+    GF_BG_LYR_SUB_0,
+    GF_BG_LYR_SUB_1,
+};
+
+const u8 ov122_021E9278[5][2] = {
+    { 0x77, 0x17 },
+    { 0x74, 0x14 },
+    { 0x71, 0x11 },
+    { 0x6E, 0x0E },
+    { 0x6B, 0x0B },
+};
+
+const TouchscreenListMenuTemplate ov122_021E9282 = {
+    1,
+    1,
+    0,
+    GF_BG_LYR_MAIN_3,
+    11,
+    0,
+    0,
+    58,
+    31,
+    104,
+};
+
+const u16 ov122_021E92A0[] = {
+    0x00A0,
+    0x00A1,
+    0x00C0,
+    0x00C1,
+    0x00E0,
+    0x00E1,
+    0x0100,
+    0x0101,
+};
+
+const MsgNoList sMenuMsgNos[] = {
+    { sMainMenuMsgNos, NELEMS(sMainMenuMsgNos) },
+    { sGameInfoMsgNos, NELEMS(sGameInfoMsgNos) },
+};
+
+const struct GraphicsModes sVoltorbFlipBgModeSet = {
+    GX_DISPMODE_GRAPHICS,
+    GX_BGMODE_0,
+    GX_BGMODE_0,
+    GX_BG0_AS_2D,
+};
+
+const u8 ov122_021E92B0[4][4] = {
+    { 0x0C, 0x08, 0x05, 0x01 },
+    { 0x0F, 0x0B, 0x06, 0x02 },
+    { 0x0E, 0x0A, 0x07, 0x04 },
+    { 0x0D, 0x09, 0x08, 0x08 },
+};
+
+const OamCharTransferParam ov122_021E92D0 = {
+    0x00000000,
+    0x00020000,
+    0x00004000,
+    GX_OBJVRAMMODE_CHAR_1D_32K,
+    GX_OBJVRAMMODE_CHAR_1D_32K,
+};
+
+const SpriteResourceCountsListUnion ov122_021E92E4 = { 2, 2, 2, 2, 0, 0 };
+
+const OamManagerParam ov122_021E92FC = { 0, 126, 0, 32, 1, 126, 0, 32 };
+
+const GraphicsBanks sVoltorbFlipGraphicsBanks = {
+    GX_VRAM_BG_128_B,
+    GX_VRAM_BGEXTPLTT_NONE,
+    GX_VRAM_SUB_BG_128_C,
+    GX_VRAM_SUB_BGEXTPLTT_NONE,
+    GX_VRAM_OBJ_128_A,
+    GX_VRAM_OBJEXTPLTT_NONE,
+    GX_VRAM_SUB_OBJ_16_I,
+    GX_VRAM_SUB_OBJEXTPLTT_NONE,
+    GX_VRAM_TEX_NONE,
+    GX_VRAM_TEXPLTT_NONE,
+};
+
+const Ov122_021E6C2C ov122_021E9344[4] = {
+    { 0x00, 0x00, 0x21 },
+    { 0x02, 0x00, 0x22 },
+    { 0x00, 0x02, 0x23 },
+    { 0x02, 0x02, 0x24 },
+};
+
+const ManagedSpriteTemplate ov122_021E93A8 = {
+    0,
+    0,
+    0,
+    0,
+    0,
+    0xFFFF,
+    NNS_G2D_VRAM_TYPE_2DSUB,
+    {
+      1,
+      1,
+      1,
+      1,
+      0,
+      0,
+      },
+    0,
+    0,
+};
+
+const ManagedSpriteTemplate ov122_021E9374 = {
+    0,
+    0,
+    0,
+    0,
+    0,
+    0xFFFF,
+    NNS_G2D_VRAM_TYPE_2DMAIN,
+    {
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      },
+    3,
+    0,
+};
+
+const WindowTemplate sVoltorbFlipWindowTemplates[17] = {
+    {
+     .bgId = 0x03,
+     .left = 0x02,
+     .top = 0x13,
+     .width = 0x1B,
+     .height = 0x04,
+     .palette = 0x0C,
+     .baseTile = 0x00A2,
+
+     },
+    {
+     .bgId = 0x03,
+     .left = 0x02,
+     .top = 0x15,
+     .width = 0x1B,
+     .height = 0x02,
+     .palette = 0x0C,
+     .baseTile = 0x00A2,
+
+     },
+    {
+     .bgId = 0x01,
+     .left = 0x19,
+     .top = 0x04,
+     .width = 0x06,
+     .height = 0x06,
+     .palette = 0x0E,
+     .baseTile = 0x010E,
+
+     },
+    {
+     .bgId = 0x01,
+     .left = 0x19,
+     .top = 0x15,
+     .width = 0x06,
+     .height = 0x02,
+     .palette = 0x0E,
+     .baseTile = 0x0132,
+
+     },
+    {
+     .bgId = 0x05,
+     .left = 0x01,
+     .top = 0x01,
+     .width = 0x1E,
+     .height = 0x02,
+     .palette = 0x0C,
+     .baseTile = 0x0001,
+
+     },
+    {
+     .bgId = 0x05,
+     .left = 0x01,
+     .top = 0x03,
+     .width = 0x1E,
+     .height = 0x02,
+     .palette = 0x0C,
+     .baseTile = 0x003D,
+
+     },
+    {
+     .bgId = 0x05,
+     .left = 0x0D,
+     .top = 0x06,
+     .width = 0x12,
+     .height = 0x03,
+     .palette = 0x0C,
+     .baseTile = 0x0079,
+
+     },
+    {
+     .bgId = 0x05,
+     .left = 0x0D,
+     .top = 0x0A,
+     .width = 0x12,
+     .height = 0x03,
+     .palette = 0x0C,
+     .baseTile = 0x00AF,
+
+     },
+    {
+     .bgId = 0x05,
+     .left = 0x01,
+     .top = 0x0E,
+     .width = 0x13,
+     .height = 0x04,
+     .palette = 0x0C,
+     .baseTile = 0x00E5,
+
+     },
+    {
+     .bgId = 0x05,
+     .left = 0x01,
+     .top = 0x13,
+     .width = 0x13,
+     .height = 0x04,
+     .palette = 0x0C,
+     .baseTile = 0x0131,
+
+     },
+    {
+     .bgId = 0x05,
+     .left = 0x01,
+     .top = 0x0F,
+     .width = 0x07,
+     .height = 0x02,
+     .palette = 0x0D,
+     .baseTile = 0x0001,
+
+     },
+    {
+     .bgId = 0x05,
+     .left = 0x01,
+     .top = 0x05,
+     .width = 0x1E,
+     .height = 0x06,
+     .palette = 0x0C,
+     .baseTile = 0x000F,
+
+     },
+    {
+     .bgId = 0x05,
+     .left = 0x0A,
+     .top = 0x0E,
+     .width = 0x15,
+     .height = 0x04,
+     .palette = 0x0C,
+     .baseTile = 0x00C3,
+
+     },
+    {
+     .bgId = 0x05,
+     .left = 0x0A,
+     .top = 0x13,
+     .width = 0x15,
+     .height = 0x04,
+     .palette = 0x0C,
+     .baseTile = 0x0117,
+
+     },
+    {
+     .bgId = 0x05,
+     .left = 0x01,
+     .top = 0x11,
+     .width = 0x1E,
+     .height = 0x06,
+     .palette = 0x0C,
+     .baseTile = 0x0001,
+
+     },
+    {
+     .bgId = 0x05,
+     .left = 0x02,
+     .top = 0x07,
+     .width = 0x06,
+     .height = 0x06,
+     .palette = 0x0D,
+     .baseTile = 0x0001,
+
+     },
+    {
+     .bgId = 0x05,
+     .left = 0x01,
+     .top = 0x0F,
+     .width = 0x1E,
+     .height = 0x08,
+     .palette = 0x0C,
+     .baseTile = 0x0025,
+
+     },
+};
+
+const BgTemplates sVoltorbFlipBgTemplates = {
+    {
+     {
+            .x = 0,
+            .y = 0,
+            .bufferSize = 0x0800,
+            .baseTile = 0,
+            .size = GF_BG_SCR_SIZE_256x256,
+            .colorMode = GX_BG_COLORMODE_16,
+            .screenBase = GX_BG_SCRBASE_0x0000,
+            .charBase = GX_BG_CHARBASE_0x04000,
+            .bgExtPltt = GX_BG_EXTPLTT_01,
+            .priority = 3,
+            .areaOver = GX_BG_AREAOVER_XLU,
+            .dummy = 0,
+            .mosaic = FALSE,
+        },
+     {
+            .x = 0,
+            .y = 0,
+            .bufferSize = 0x0800,
+            .baseTile = 0,
+            .size = GF_BG_SCR_SIZE_256x256,
+            .colorMode = GX_BG_COLORMODE_16,
+            .screenBase = GX_BG_SCRBASE_0x0800,
+            .charBase = GX_BG_CHARBASE_0x08000,
+            .bgExtPltt = GX_BG_EXTPLTT_01,
+            .priority = 2,
+            .areaOver = GX_BG_AREAOVER_XLU,
+            .dummy = 0,
+            .mosaic = FALSE,
+        },
+     {
+            .x = 0,
+            .y = 0,
+            .bufferSize = 0x0800,
+            .baseTile = 0,
+            .size = GF_BG_SCR_SIZE_256x256,
+            .colorMode = GX_BG_COLORMODE_16,
+            .screenBase = GX_BG_SCRBASE_0x1000,
+            .charBase = GX_BG_CHARBASE_0x0c000,
+            .bgExtPltt = GX_BG_EXTPLTT_01,
+            .priority = 1,
+            .areaOver = GX_BG_AREAOVER_XLU,
+            .dummy = 0,
+            .mosaic = FALSE,
+        },
+     {
+            .x = 0,
+            .y = 0,
+            .bufferSize = 0x0800,
+            .baseTile = 0,
+            .size = GF_BG_SCR_SIZE_256x256,
+            .colorMode = GX_BG_COLORMODE_16,
+            .screenBase = GX_BG_SCRBASE_0x1800,
+            .charBase = GX_BG_CHARBASE_0x10000,
+            .bgExtPltt = GX_BG_EXTPLTT_01,
+            .priority = 0,
+            .areaOver = GX_BG_AREAOVER_XLU,
+            .dummy = 0,
+            .mosaic = FALSE,
+        },
+     {
+            .x = 0,
+            .y = 0,
+            .bufferSize = 0x0800,
+            .baseTile = 0,
+            .size = GF_BG_SCR_SIZE_256x256,
+            .colorMode = GX_BG_COLORMODE_16,
+            .screenBase = GX_BG_SCRBASE_0x0000,
+            .charBase = GX_BG_CHARBASE_0x04000,
+            .bgExtPltt = GX_BG_EXTPLTT_01,
+            .priority = 1,
+            .areaOver = GX_BG_AREAOVER_XLU,
+            .dummy = 0,
+            .mosaic = FALSE,
+        },
+     {
+            .x = 0,
+            .y = 0,
+            .bufferSize = 0x0800,
+            .baseTile = 0,
+            .size = GF_BG_SCR_SIZE_256x256,
+            .colorMode = GX_BG_COLORMODE_16,
+            .screenBase = GX_BG_SCRBASE_0x0800,
+            .charBase = GX_BG_CHARBASE_0x08000,
+            .bgExtPltt = GX_BG_EXTPLTT_01,
+            .priority = 0,
+            .areaOver = GX_BG_AREAOVER_XLU,
+            .dummy = 0,
+            .mosaic = FALSE,
+        },
+     }
+};
 
 static void RenderCoinPayoutScreen(VoltorbFlipAppWork *work) {
     BgClearTilemapBufferAndCommit(work->bgConfig, 5);
@@ -1181,7 +1583,7 @@ static int ov122_021E6E10(u8 a0, u8 a1) {
     GF_ASSERT(a1 < 2);
     GF_ASSERT(a0 < 5);
 
-    return ov122_021E9278.unk0[a0][a1];
+    return ov122_021E9278[a0][a1];
 }
 
 static void ov122_021E6E34(u16 *a0, int a1) {
@@ -1483,7 +1885,7 @@ void ov122_021E7488(VoltorbFlipAppWork *a0, int a1) {
     const MsgNoList *ptr = &sMenuMsgNos[a1];
     GF_ASSERT(a0->bgConfig != 0);
 
-    temp1.template = ov122_021E9278.menuTemplate;
+    temp1.template = ov122_021E9282;
 
     temp1.listMenuItems = a0->menuItems[a1];
     temp1.bgConfig = a0->bgConfig;
