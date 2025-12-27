312d2ac0ab26a104ed76d0f6f4257c7ac1f5b9b9
PikalaxALT <PikalaxALT@users.noreply.github.com>
2025-12-27 16:50:15 -0500

    fix soulsilver build

--- files/application/zukanlist/zukan_data/zukan_enc.mk
+++ files/application/zukanlist/zukan_data/zukan_enc.mk
@@ -4,7 +4,7 @@ ZUKAN_ENC_JSON := $(ZUKAN_ENC_PREF).json
 ZUKAN_ENC_JSON_TXT := $(ZUKAN_ENC_PREF).json.txt
 
 $(ZUKAN_ENC_NARC): %.narc: $(ZUKAN_ENC_JSON) $(ZUKAN_ENC_JSON_TXT)
-	$(JSONPROC) -D$(GAME_VERSION)=1 $^ $*.s
+	$(JSONPROC) $^ $*.s
 	$(WINE) $(MWAS) $(MWASFLAGS) -DPM_ASM -o $*.o $*.s
 	$(O2NARC) $*.o $@ -n -p 0x00
 	@$(RM) $*.o $*.s
