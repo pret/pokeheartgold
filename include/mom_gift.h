7280b4e3ad06d1433fccda83aa8e0cbd5efca440
Kurokamori <abagaildavid.me@gmail.com>
2026-01-11 19:52:52 -0600

    unk_020931C4 to C as mom_gift.c/h
    file handles part of the mom gift system, moved it over and updated it's call location in battle_setup

---
+++ include/mom_gift.h
@@ -0,0 +1,8 @@
+#ifndef POKEHEARTGOLD_MOM_GIFT_H
+#define POKEHEARTGOLD_MOM_GIFT_H
+
+#include "save_pokegear.h"
+
+BOOL MomGift_TryQueueOnBalanceChange(PhoneCallPersistentState *savings, u32 newBalance, u32 oldBalance);
+
+#endif // POKEHEARTGOLD_MOM_GIFT_H
