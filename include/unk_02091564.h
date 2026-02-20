8533b44f9edbbbdfe25f687f4e5c7c57e575343e
ferwardle <>
2026-02-20 15:26:10 -0600

    Decompile unk_02091564

--- include/unk_02091564.h
+++ include/unk_02091564.h
@@ -1,8 +1,27 @@
 #ifndef POKEHEARTGOLD_UNK_02091564_H
 #define POKEHEARTGOLD_UNK_02091564_H
 
-#include "script.h"
+#include "global.h"
+#include "overlay_manager.h"
 
-void sub_02091574(void *a0);
+extern const OverlayManagerTemplate gApplication_OakSpeech;
+
+typedef struct UnkStruct_02091564 {
+    void* func1;
+    void* func2;
+    void* func3;
+} UnkStruct_02091564;
+
+void sub_02091574(void* arg0);
+s32 sub_02091588(void);
+s32 sub_02091590(void);
+s32 sub_02091598(void);
+s32 sub_02091564();
+s32 sub_02091568();
+void sub_02091570(void);
+s32 sub_020915A0();
+s32 sub_020915A4();
+s32 sub_020915A8();
+s32 sub_020915AC();
 
 #endif // POKEHEARTGOLD_UNK_02091564_H
