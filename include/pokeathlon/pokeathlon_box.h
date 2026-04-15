c17438b4a5216df38031a43325777db99e7b1d66
NathanFallet <contact@nathanfallet.me>
2026-04-15 23:17:28 +0200

    checkpoint, good night

--- include/pokeathlon/pokeathlon_box.h
+++ include/pokeathlon/pokeathlon_box.h
@@ -3,8 +3,19 @@
 
 #include "global.h"
 
-BOOL ov97_0221E5C0();
+#include "overlay_manager.h"
 
+typedef struct PokeathlonBox {
+    u32 unk0;
+    u32 unk4;
+    u32 unk8;
+    u32 unkC;
+    u8 filler10[24];
+    BOOL unk28;
+} PokeathlonBox;
+
+BOOL ov97_0221E5C0();
+BOOL ov97_0221E5D4(OverlayManager *manager);
 
 u32 ov97_0221E6DC();
 
--- src/pokeathlon/pokeathlon_box.c
+++ src/pokeathlon/pokeathlon_box.c
@@ -1,8 +1,66 @@
 #include "pokeathlon/pokeathlon_box.h"
 
+#include "heap.h"
+#include "unk_0200FA24.h"
+
+// functions in asm:
+int sub_020935E0(int);
+void ov97_0221E98C(PokeathlonBox*, void*);
+void sub_02093A40(u32);
+void ov97_0221F010(u32);
+
 BOOL ov97_0221E5C0() {
     if (ov97_0221E6DC() != 0) {
         return TRUE;
     }
+    return FALSE;
+}
+
+BOOL ov97_0221E5D4(OverlayManager *manager) {
+    PokeathlonBox* data = OverlayManager_GetData(manager);
+    
+    switch (data->unk4) {
+        case 0:
+            data->unk4 = 1;
+            break;
+
+        case 1:
+            BeginNormalPaletteFade(0, 1, 1, 0, 6, 1, HEAP_ID_POKEATHLON);
+            data->unk4 = 2;
+            break;
+
+        case 2:
+            if (IsPaletteFadeFinished()) {
+                data->unk4 = 3;
+            }
+            break;
+
+        case 3:
+            int res = sub_020935E0(data->unk8);
+            if (res == 2) {
+                data->unk28 = FALSE;
+                data->unk4 = 4;
+            } else if (res == 3) {
+                data->unk28 = TRUE;
+                data->unk4 = 4;
+            }
+            break;
+
+        case 4:
+            BeginNormalPaletteFade(0, 0, 0, 0, 6, 1, HEAP_ID_POKEATHLON);
+            data->unk4 = 5;
+            break;
+
+        case 5:
+            if (IsPaletteFadeFinished()) {
+                void *args = OverlayManager_GetArgs(manager);
+                ov97_0221E98C(data, args);
+                return TRUE;
+            }
+            break;
+    }
+
+    sub_02093A40(data->unk8);
+    ov97_0221F010(data->unkC);
     return FALSE;
 }
