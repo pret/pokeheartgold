f1d4f1a8371dab3ca8cc47449aba137f370187ec
PikalaxALT <PikalaxALT@users.noreply.github.com>
2026-08-14 08:11:40 -0400

    ov04_02255140

--- src/field/gymmick/blackthorn.c
+++ src/field/gymmick/blackthorn.c
@@ -3,11 +3,28 @@
 #include "overlay_04.h"
 #include "unk_02054648.h"
 
+typedef struct BlackthornGymXZPoint {
+    int x;
+    int z;
+} BlackthornGymXZPoint;
+
 typedef struct BlackthornGymmickPlatformData {
-    u8 filler_000[0x7];
+    int unk_000;
+    u8 unk_004;
+    u8 unk_005;
+    u8 unk_006;
     u8 unk_007;
-    u8 filler_008[0x1D8];
-    u32 unk_1E0[18][2];
+    BlackthornGymXZPoint unk_008;
+    BlackthornGymXZPoint unk_010;
+    BlackthornGymXZPoint unk_018;
+    BlackthornGymXZPoint unk_020;
+    BlackthornGymXZPoint unk_028[8];
+    BlackthornGymXZPoint unk_068[8];
+    BlackthornGymXZPoint unk_0A8[5];
+    BlackthornGymXZPoint unk_0D0[5];
+    BlackthornGymXZPoint unk_0F8[24];
+    BlackthornGymXZPoint unk_1B8[5];
+    BlackthornGymXZPoint unk_1E0[18];
 } BlackthornGymmickPlatformData; // size: 0x270
 
 typedef struct BlackthornGymmickLocalData {
@@ -16,6 +33,7 @@ typedef struct BlackthornGymmickLocalData {
 } BlackthornGymmickLocalData; // size: 0x754
 
 void ov04_02255140(u8 i, u8 rot, u16 x, u16 z, BlackthornGymmickPlatformData *platform);
+void ov04_022554FC(u8 rot, u16 x, u16 z, BlackthornGymmickPlatformData *platform);
 BOOL ov04_02255708(BlackthornGymmickLocalData *localData, u16 x, u16 z);
 
 void GymmickInit_Blackthorn(FieldSystem *fieldSystem) {
@@ -61,7 +79,7 @@ BOOL GymmickCheckCollision_Blackthorn(FieldSystem *fieldSystem, u32 tileX, u32 t
     for (int i = 0; i < 3; ++i) {
         BlackthornGymmickPlatformData *platform = &localData->platforms[i];
         for (int j = 0; j < 18; ++j) {
-            if (tileX == platform->unk_1E0[j][0] && tileZ == platform->unk_1E0[j][1]) {
+            if (tileX == platform->unk_1E0[j].x && tileZ == platform->unk_1E0[j].z) {
                 *isColliding = FALSE;
                 return TRUE;
             }
@@ -72,4 +90,141 @@ BOOL GymmickCheckCollision_Blackthorn(FieldSystem *fieldSystem, u32 tileX, u32 t
         return TRUE;
     }
     return FALSE;
+}
+
+// This function is weird. I promise, every oddity is required to match.
+// Scratch: https://decomp.me/scratch/6ql5q
+void ov04_02255140(u8 index, u8 rot, u16 x, u16 z, BlackthornGymmickPlatformData *platform) {
+    BlackthornGymXZPoint sp1C;
+
+    platform->unk_008.x = x;
+    platform->unk_008.z = z;
+    platform->unk_010.x = x + 1;
+    platform->unk_010.z = z;
+    platform->unk_018.x = x - 1;
+    platform->unk_018.z = z;
+    platform->unk_020.x = x;
+    platform->unk_020.z = z;
+
+    if (index != 1) {
+        // Declaration here is required to match
+        int i, j;
+        platform->unk_000 = 0;
+        platform->unk_004 = 5;
+        platform->unk_005 = 7;
+        sp1C.x = x;
+        sp1C.z = z;
+        sp1C.x += 3;
+        sp1C.z -= 2;
+        for (i = 0; i < 7; ++i) {
+            platform->unk_028[i] = sp1C;
+            platform->unk_028[i].z += i;
+        }
+        sp1C.x = x;
+        sp1C.z = z;
+        sp1C.x -= 3;
+        sp1C.z -= 2;
+        for (i = 0; i < 7; ++i) {
+            platform->unk_068[i] = sp1C;
+            platform->unk_068[i].z += i;
+        }
+        sp1C.x = x;
+        sp1C.z = z;
+        sp1C.x -= 2;
+        sp1C.z += 5;
+        for (i = 0; i < 5; ++i) {
+            platform->unk_0A8[i] = sp1C;
+            platform->unk_0A8[i].x += i;
+        }
+        sp1C.x = x;
+        sp1C.z = z;
+        sp1C.x -= 2;
+        sp1C.z -= 3;
+        for (i = 0; i < 5; ++i) {
+            platform->unk_0D0[i] = sp1C;
+            platform->unk_0D0[i].x += i;
+        }
+        for (i = 0; i < 2; ++i) {
+            sp1C.x = x - 4;
+            sp1C.z = z + 4;
+            sp1C.x -= i;
+            for (j = 0; j < 7; ++j) {
+                platform->unk_0F8[i * 7 + j] = sp1C;
+                platform->unk_0F8[i * 7 + j].z -= j;
+            }
+        }
+        for (i = 0; i < platform->unk_004; ++i) {
+            sp1C.x = x + 3;
+            sp1C.z = z - 2;
+            platform->unk_1B8[i] = sp1C;
+            platform->unk_1B8[i].z += i;
+        }
+    } else {
+        // Declaration here is required to match
+        int i, j;
+        platform->unk_000 = 1;
+        platform->unk_004 = 4;
+        platform->unk_005 = 8;
+        sp1C.x = x;
+        sp1C.z = z;
+        sp1C.x += 2;
+        sp1C.z -= 2;
+        for (i = 0; i < 8; ++i) {
+            platform->unk_028[i] = sp1C;
+            platform->unk_028[i].z += i;
+        }
+        sp1C.x = x;
+        sp1C.z = z;
+        sp1C.x -= 3;
+        sp1C.z -= 2;
+        for (i = 0; i < 8; ++i) {
+            platform->unk_068[i] = sp1C;
+            platform->unk_068[i].z += i;
+        }
+        sp1C.x = x;
+        sp1C.z = z;
+        sp1C.x -= 2;
+        sp1C.z += 6;
+        for (i = 0; i < 4; ++i) {
+            platform->unk_0A8[i] = sp1C;
+            platform->unk_0A8[i].x += i;
+        }
+        sp1C.x = x;
+        sp1C.z = z;
+        sp1C.x -= 2;
+        sp1C.z -= 3;
+        for (i = 0; i < 4; ++i) {
+            platform->unk_0D0[i] = sp1C;
+            platform->unk_0D0[i].x += i;
+        }
+        for (i = 0; i < 3; ++i) {
+            sp1C.x = x - 4;
+            sp1C.z = z + 5;
+            sp1C.x -= i;
+            for (j = 0; j < 8; ++j) {
+                platform->unk_0F8[i * 8 + j] = sp1C;
+                platform->unk_0F8[i * 8 + j].z -= j;
+            }
+        }
+        for (i = 0; i < platform->unk_004; ++i) {
+            sp1C.x = x + 3;
+            sp1C.z = z - 2;
+            platform->unk_1B8[i] = sp1C;
+            platform->unk_1B8[i].z += i;
+        }
+    }
+    {
+        // Declaration here is required to match
+        int i, j;
+        sp1C.x = x - 1;
+        sp1C.z = z - 1;
+        for (i = 0; i < 6; ++i) {
+            for (j = 0; j < 3; ++j) {
+                platform->unk_1E0[i * 3 + j].x = sp1C.x + j;
+                platform->unk_1E0[i * 3 + j].z = sp1C.z + i;
+            }
+        }
+    }
+    platform->unk_006 = rot;
+    ov04_022554FC(rot, x, z, platform);
 }
