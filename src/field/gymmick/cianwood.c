b718052e8d5dca7c6af6943f065a2554eeb8b82f
PikalaxALT <PikalaxALT@users.noreply.github.com>
2026-08-16 15:29:56 -0400

    ov04_022560D4

--- src/field/gymmick/cianwood.c
+++ src/field/gymmick/cianwood.c
@@ -6,9 +6,9 @@ typedef struct CianwoodGymmickLocalData {
 } CianwoodGymmickLocalData;
 
 typedef struct CianwoodGymmickWinchTaskData {
-    int unk_0;
-    int unk_4;
-    int unk_8;
+    int state;
+    int unused;
+    GFCameraTranslationWrapper *translationWrapper;
 } CianwoodGymmickWinchTaskData;
 
 void ov04_0225609C(CianwoodGymmickLocalData *localData);
@@ -61,7 +61,78 @@ BOOL ov04_02256058(FieldSystem *fieldSystem) {
 void ov04_0225609C(CianwoodGymmickLocalData *localData) {
     GymmickUnion *gymmickUnion = Save_Gymmick_AssertMagic_GetData(Save_GetGymmickPtr(FieldSystem_GetSaveData(localData->fieldSystem)), GYMMICK_CIANWOOD);
     CianwoodGymmickWinchTaskData *taskData = Heap_AllocAtEnd(HEAP_ID_FIELD2, sizeof(CianwoodGymmickWinchTaskData));
-    taskData->unk_0 = 0;
+    taskData->state = 0;
     gymmickUnion->cianwood.winch = TRUE;
     TaskManager_Call(localData->fieldSystem->taskman, ov04_022560D4, taskData);
+}
+
+BOOL ov04_022560D4(TaskManager *taskman) {
+    FieldSystem *fieldSystem = TaskManager_GetFieldSystem(taskman);
+    CianwoodGymmickWinchTaskData *taskData = TaskManager_GetEnvironment(taskman);
+    LocalMapObject *playerObject = PlayerAvatar_GetMapObject(fieldSystem->playerAvatar);
+
+    switch (taskData->state) {
+    case 0: {
+        CameraTranslationPathTemplate sp44;
+        CameraAngle cameraAngle = Camera_GetAngle(fieldSystem->camera);
+        sp44.angleX = cameraAngle.x;
+        sp44.perspectiveAngle = Camera_GetPerspectiveAngle(fieldSystem->camera);
+        sp44.position = (VecFx32) {
+            50 * FX32_ONE,
+            0,
+            58 * FX32_ONE,
+        };
+        sp44.distance = Camera_GetDistance(fieldSystem->camera);
+        taskData->translationWrapper = CreateCameraTranslationWrapper(HEAP_ID_FIELD1, fieldSystem->camera);
+        SetCameraTranslationPath(taskData->translationWrapper, &sp44, 24);
+        ++taskData->state;
+    } break;
+    case 1:
+        if (IsCameraTranslationFinished(taskData->translationWrapper)) {
+            DeleteCameraTranslationWrapper(taskData->translationWrapper);
+            ++taskData->state;
+        }
+        break;
+    case 2: {
+        int sp0 = ov04_02256278(fieldSystem);
+        MapPropAnimationData *animData;
+        for (int i = 0; i < sp0; ++i) {
+            animData = MapPropAnimationManager_GetAnimationData(174, i, fieldSystem->mapPropAnimationManager);
+            MapPropAnimationData_SetAnimationLoopCount(animData, 1);
+            MapPropAnimationData_GoToFirstAnimationFrame(animData);
+        }
+        animData = MapPropAnimationManager_GetAnimationData(175, 0, fieldSystem->mapPropAnimationManager);
+        MapPropAnimationData_SetAnimationLoopCount(animData, 1);
+        MapPropAnimationData_GoToFirstAnimationFrame(animData);
+        ++taskData->state;
+    } break;
+    case 3:
+        if (MapPropAnimationData_IsAnimationLoopFinished(MapPropAnimationManager_GetAnimationData(175, 0, fieldSystem->mapPropAnimationManager))) {
+            ++taskData->state;
+        }
+        break;
+    case 4: {
+        CameraTranslationPathTemplate sp30;
+        VecFx32 sp24 = { 0, 0, 0 };
+        CameraAngle angle = Camera_GetAngle(fieldSystem->camera);
+        sp30.angleX = angle.x;
+        sp30.perspectiveAngle = Camera_GetPerspectiveAngle(fieldSystem->camera);
+        sp30.position = sp24;
+        sp30.distance = Camera_GetDistance(fieldSystem->camera);
+        taskData->translationWrapper = CreateCameraTranslationWrapper(HEAP_ID_FIELD1, fieldSystem->camera);
+        SetCameraTranslationPath(taskData->translationWrapper, &sp30, 24);
+        ++taskData->state;
+    } break;
+    case 5:
+        if (IsCameraTranslationFinished(taskData->translationWrapper)) {
+            DeleteCameraTranslationWrapper(taskData->translationWrapper);
+            ++taskData->state;
+        }
+        break;
+    case 6:
+        Heap_Free(taskData);
+        return TRUE;
+    }
+
+    return FALSE;
 }
