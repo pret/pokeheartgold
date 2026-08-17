9711b3fa44fb9431d6d2097ad21177c4399294c0
PikalaxALT <PikalaxALT@users.noreply.github.com>
2026-08-16 20:41:15 -0400

    ov04_0225640C

--- src/field/gymmick/vermilion.c
+++ src/field/gymmick/vermilion.c
@@ -5,6 +5,17 @@ typedef struct VermilionGymmickLocalData {
     FieldSystem *fieldSystem;
 } VermilionGymmickLocalData;
 
+typedef struct VermilionGymmickLockActionTaskData {
+    int unk_0;
+    LocalMapObject *unk_4[3];
+    int unk_10;
+    u16 unk_14;
+    u8 unk_16;
+    u8 unk_17;
+} VermilionGymmickLockActionTaskData;
+
+BOOL ov04_022564A0(TaskManager *taskman);
+
 void GymmickInit_Vermilion(FieldSystem *fieldSystem) {
     GymmickUnion *gymmickUnion = Save_Gymmick_AssertMagic_GetData(Save_GetGymmickPtr(FieldSystem_GetSaveData(fieldSystem)), GYMMICK_VERMILION);
     fieldSystem->unk4->unk24 = Heap_Alloc(HEAP_ID_FIELD1, sizeof(VermilionGymmickLocalData));
@@ -50,4 +61,37 @@ u8 ov04_022563C4(FieldSystem *fieldSystem, u8 canId) {
     }
 
     return canId == gymmickUnion->vermilion.switches[0];
+}
+
+void ov04_0225640C(FieldSystem *fieldSystem, u8 lockno, u8 relock) {
+    GymmickUnion *gymmickUnion = Save_Gymmick_AssertMagic_GetData(Save_GetGymmickPtr(FieldSystem_GetSaveData(fieldSystem)), GYMMICK_VERMILION);
+    VermilionGymmickLockActionTaskData *taskData = Heap_AllocAtEnd(HEAP_ID_FIELD2, sizeof(VermilionGymmickLockActionTaskData));
+
+    switch (lockno) {
+    case 0:
+        taskData->unk_10 = 199;
+        if (relock) {
+            taskData->unk_16 = 2;
+            TaskManager_Call(fieldSystem->taskman, ov04_022564A0, taskData);
+            gymmickUnion->vermilion.gates[0] = FALSE;
+        } else {
+            taskData->unk_16 = 1;
+            TaskManager_Call(fieldSystem->taskman, ov04_022564A0, taskData);
+            gymmickUnion->vermilion.gates[0] = TRUE;
+        }
+        break;
+    case 1:
+        taskData->unk_10 = 200;
+        if (relock) {
+            GF_ASSERT(FALSE);
+            Heap_Free(taskData);
+        } else {
+            taskData->unk_16 = 1;
+            TaskManager_Call(fieldSystem->taskman, ov04_022564A0, taskData);
+            gymmickUnion->vermilion.gates[1] = TRUE;
+        }
+        break;
+    default:
+        GF_ASSERT(FALSE);
+    }
 }
